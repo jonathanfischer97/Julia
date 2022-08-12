800c69fb05449a40f1cb3c8e678fe6cc72eac967
jonathanfischer97 <jfisch27@jhu.edu>
2022-08-12 11:22:50 -0400

    more catalyst tests

--- test.jl
+++ test.jl
@@ -2,7 +2,7 @@ using DifferentialEquations
 using Plots
 using Distributed
 using Catalyst
-using Graphviz_jll
+#using Graphviz_jll
 
 rn = @reaction_network begin
     ka1, L+K --> LK
@@ -28,7 +28,7 @@ end ka1 kb1 kcat1 ka2 kb2 ka3 kb3 ka4 kb4 ka5 kb5 kcat5 ka6 kb6 kcat6 ka7 kb7 kc
 y = 608.5222549026549
 
 
-p = (:ka1 => 0.5399439956871153, :kb1 => 32.49731125365913, :kcat1 => 459.0713154241908, :ka2 => 0.6703697205723796, :kb2 => 98.55386776137301, :ka3 => 66.94761309866233, :kb3 => 187.88476784879947,
+p = Dict(:ka1 => 0.5399439956871153, :kb1 => 32.49731125365913, :kcat1 => 459.0713154241908, :ka2 => 0.6703697205723796, :kb2 => 98.55386776137301, :ka3 => 66.94761309866233, :kb3 => 187.88476784879947,
     :ka4 => 0.732829082837601, :kb4 => 0.5932803922080772, :ka5 => 0.8833353263653272, :kb5 => 79.8349667706061, :kcat5 => 98.51067194455857, :ka6 => 0.5399439956871153*y, :kb6 => 32.49731125365913,
     :kcat6 => 459.0713154241908, :ka7 => 0.8833353263653272*y, :kb7 => 79.8349667706061, :kcat7 => 98.51067194455857)
 
@@ -40,4 +40,32 @@ u0 = [:L => 8.973816043747753, :Lp => 0, :K => 0.6170991018130821, :P => 0.79341
 oprob = ODEProblem(rn, u0, tspan, p)
 osol = solve(oprob, Tsit5())
 
-Graph(rn)
+species(rn)
+
+## for bifurcation analysis
+bif_par = :ka1 
+p_span = (0.1, 1.)
+plot_var = :L
+
+p_bstart = copy(p)
+p_bstart[bif_par] = p_span[1]
+
+bifprob = ODEProblem(rn, u0, (0.0,0.0), p_bstart; jac = true)
+F = (u,p) -> bifprob.f(u, p, 0)
+J = (u,p) -> bifprob.f.jac(u, p, 0)
+
+conservedequations(rn)
+
+incidencematgraph(rn)
+
+subnets = subnetworks(rn)
+isweaklyreversible(rn, subnets)
+
+ns = convert(NonlinearSystem,rn)
+
+subs = Dict(Pair.(ModelingToolkit.parameters(ns),last.(p)))
+new_eqs = map(eq -> substitute(eq.rhs,subs), equations(ns))
+
+lcs = linkageclasses(rn)
+
+deficiency(rn)
