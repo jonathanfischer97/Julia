a31bc996ccf1455efd1c6ae190496f6aa93530f2
jonathanfischer97 <jfisch27@jhu.edu>
2022-08-29 23:08:52 -0400

    got cost func and gradient working. No optimize

--- ODEs.jl
+++ ODEs.jl
@@ -14,9 +14,8 @@ using PyCall
 #np = pyimport("numpy")
 #peak = pyimport("peakutils")
 
-#define system of ODEs, out of place (returns new array allocation)
-#would be my_ode_solver!(du, u, p, t) for in place, where du is interpreted as a preallocated array that is mutated
-function my_ode_solver(u::Array, p::Array, t)
+#changed to in-place definition 
+function my_ode_solver(du, u, p, t)
     #parameter assignment, unpacked from p
     ka1,kb1,kcat1,ka2,kb2,ka3,kb3,ka4,kb4,ka5,kb5,kcat5,y = p
     ka6,kb6,kcat6 = ka1,kb1,kcat1
@@ -25,19 +24,19 @@ function my_ode_solver(u::Array, p::Array, t)
     #initial condition assignment, unpacked from u
     L,Lp,K,P,LK,A,LpA,LpAK,LpAP,LpAPLp,LpAKL,LpP = u
 
-    dL = (kb1*LK) - (ka1*L*K) + (kcat5*LpAPLp) + (kb6*LpAKL) - (y*ka6*LpAK*L) + (kcat7*LpP)
-    dLp = (kcat1*LK) + (kb2*LpA) - (ka2*Lp*A) + (kb5*LpAPLp) - (y*ka5*Lp*LpAP) + (kcat6*LpAKL) - (ka7*Lp*P) + (kb7*LpP)
-    dK = (kb1*LK) - (ka1*L*K) + (kcat1*LK) + (kb3*LpAK) - (ka3*LpA*K)
-    dP = (kb4*LpAP) - (ka4*LpA*P) - (ka7*Lp*P) + (kb7*LpP) + (kcat7*LpP)
-    dLK = (ka1*L*K) - (kb1*LK) - (kcat1*LK)
-    dA = (kb2*LpA) - (ka2*Lp*A)
-    dLpA = (ka2*Lp*A) - (kb2*LpA) + (kb3*LpAK) - (ka3*LpA*K) + (kb4*LpAP) - (ka4*LpA*P)
-    dLpAK = (ka3*LpA*K) - (kb3*LpAK) + (kb6*LpAKL) - (y*ka6*LpAK*L) + (kcat6*LpAKL)
-    dLpAP = (ka4*LpA*P) - (kb4*LpAP) + (kb5*LpAPLp) - (y*ka5*LpAP*Lp) + (kcat5*LpAPLp)
-    dLpAPLp = (y*ka5*LpAP*Lp) - (kb5*LpAPLp) - (kcat5*LpAPLp)
-    dLpAKL = (y*ka6*LpAK*L) - (kb6*LpAKL) - (kcat6*LpAKL)
-    dLpP = (ka7*Lp*P) - (kb7*LpP) - (kcat7*LpP)
-    [dL,dLp,dK,dP,dLK,dA,dLpA,dLpAK,dLpAP,dLpAPLp,dLpAKL,dLpP]
+    du[1] = (kb1*LK) - (ka1*L*K) + (kcat5*LpAPLp) + (kb6*LpAKL) - (y*ka6*LpAK*L) + (kcat7*LpP)
+    du[2] = (kcat1*LK) + (kb2*LpA) - (ka2*Lp*A) + (kb5*LpAPLp) - (y*ka5*Lp*LpAP) + (kcat6*LpAKL) - (ka7*Lp*P) + (kb7*LpP)
+    du[3] = (kb1*LK) - (ka1*L*K) + (kcat1*LK) + (kb3*LpAK) - (ka3*LpA*K)
+    du[4] = (kb4*LpAP) - (ka4*LpA*P) - (ka7*Lp*P) + (kb7*LpP) + (kcat7*LpP)
+    du[5] = (ka1*L*K) - (kb1*LK) - (kcat1*LK)
+    du[6] = (kb2*LpA) - (ka2*Lp*A)
+    du[7] = (ka2*Lp*A) - (kb2*LpA) + (kb3*LpAK) - (ka3*LpA*K) + (kb4*LpAP) - (ka4*LpA*P)
+    du[8] = (ka3*LpA*K) - (kb3*LpAK) + (kb6*LpAKL) - (y*ka6*LpAK*L) + (kcat6*LpAKL)
+    du[9] = (ka4*LpA*P) - (kb4*LpAP) + (kb5*LpAPLp) - (y*ka5*LpAP*Lp) + (kcat5*LpAPLp)
+    du[10] = (y*ka5*LpAP*Lp) - (kb5*LpAPLp) - (kcat5*LpAPLp)
+    du[11] = (y*ka6*LpAK*L) - (kb6*LpAKL) - (kcat6*LpAKL)
+    du[12] = (ka7*Lp*P) - (kb7*LpP) - (kcat7*LpP)
+    #[dL,dLp,dK,dP,dLK,dA,dLpA,dLpAK,dLpAP,dLpAPLp,dLpAKL,dLpP]
 end
 
 #parameter list
@@ -54,6 +53,7 @@ u0 = [0., 3.0, 0.2, 0.3, 0., 0.6, 0., 0., 0., 0., 0., 0.]
 tspan = (0., 100.)
 
 #construct ODE problem from constructor, {false} means out of place 
+
 prob = ODEProblem{false}(my_ode_solver,u0,tspan,p)
 
 sol = solve(prob, Tsit5()) #solve with non-stiff Tsit5 alg
@@ -70,9 +70,12 @@ du01,dp1 = Zygote.gradient((u0,p)->sum(solve(prob,Tsit5(),u0=u0,p=p,saveat=0.1,s
 ## COST FUNCTIONS and dependencies 
 function getDif(indexes, arrayData)
     arrLen = length(indexes)
+    println(1)
     sum = 0
     for (i, ind) in enumerate(indexes)
-        if i == arrLen - 1
+        println(i)
+        if i == arrLen
+            println(3)
             break 
         end
         sum += arrayData[ind] - arrayData[indexes[i+1]]
@@ -148,11 +151,8 @@ function testcost(p)
         return 0
     end
     std = getSTD(indexes, fftData, 1)
-    diff = getDif(indexes, fftData) #PROBLEM, BoundsError. Try a different param combo
+    diff = getDif(indexes, fftData)
     return std + diff
-    #indexes[1]
-    #Tuple(x)[1]
-    
 end
 
 dcostTwo = Zygote.gradient(testcost, p)
@@ -163,25 +163,30 @@ dcostTwo = Zygote.gradient(testcost, p)
 
 
 #IGNORE 
-function optimize(init,tend)
-    function costTwo(y)
-        Y = solve(remake(oprob, tspan = (0.,tend), p = y), Tsit5())
+function optimize(p_init,tend)
+    function testcost(p)
+        Y = Array(solve(remake(prob, tspan = (0., tend), p=p), Tsit5()))
         p1 = Y[1,:]
-        fftData = getFrequencies(p1)
-
-        indexes, _ = findmaxima(fftData) 
+        fftData = getFrequencies1(p1)
+        try 
+            indexes = findlocalmaxima(fftData)[1]
+        catch 
+            indexes = findlocalmaxima(fftData)
+        end
+        indexes = findlocalmaxima(fftData)
         if length(indexes) == 0
             return 0
         end
         std = getSTD(indexes, fftData, 1)
         diff = getDif(indexes, fftData)
-        std + diff
+        return -(std + diff)
     end
-    return DiffEqFlux.sciml_train(costTwo,init,ADAM(0.1),maxiters = 100)
+    return DiffEqFlux.sciml_train(testcost,p_init,ADAM(0.1),maxiters = 100)
 end
 
-#didn't work
-estimate = optimize([0.,3.,0.2,0.3,0.,0.6,0.,0.,0.,0.,0.,0.],10.).minimizer
+#Works, but AD doesn't apparently, used numerical differentaiton 
+estimate = optimize([0.,3.,0.2,0.3,0.,0.6,0.,0.,0.,0.,0.,0.,0.],10.).minimizer
+i_sol = solve(prob)
 
 #worked but returned all NaN
 du01,dp1 = gradient((u0,p)->sum(solve(oprob,Tsit5(),u0=u0,p=p,saveat=0.1,sensealg=ReverseDiffAdjoint())),u0,p)
