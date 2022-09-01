c6642ac31813aa767b63408fb669ee807512b77f
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2022-08-31 20:30:35 -0400

    fixed discrepancy between odes/rxns

--- Simple5.jl
+++ Simple5.jl
@@ -2,11 +2,12 @@ using DifferentialEquations
 using Plots
 using Catalyst
 
+theme(:solarized)
 
 #changed to in-place definition 
 function my_ode_solver(du, u, p, t)
     #parameter assignment, unpacked from p
-    ka1,kb1,kcat1,ka2,kb2,ka3,kb3,ka4,kb4,ka5,kb5,kcat5,ka6,kb6,ka7,kb7,kcat7,y = p
+    ka1,kb1,kcat1,ka2,kb2,ka3,kb3,ka4,kb4,kcat4,ka5,kb5,kcat5,ka6,kb6,ka7,kb7,kcat7,y = p
 
     #initial condition assignment, unpacked from u
     L,Lp,K,LK,A,LpA,LpAK,LpAKL,P,LpP,LpAP,LpAPLp = u
@@ -35,37 +36,38 @@ p = [0.47375415262252124, 70.82403936369272, 300.346311110198, 0.624675949351274
 u0 = [200., 50., 100., 0., 150., 0., 0., 0., 100., 0., 0., 0.]
 
 #timespan for integration
-tspan = (0., 100.)
+tspan = (0., 10.)
 
 #construct ODE problem from constructor, {false} means out of place 
 
 prob = ODEProblem(my_ode_solver,u0,tspan,p)
 
 sol = solve(remake(prob, tspan = (0.,10.)), Tsit5()) #solve with non-stiff Tsit5 alg
-plot(sol) #time series plot example
+plot(sol,size = (1000,700)) #time series plot example
+
+
+##REACTION NETWORK
+param_symbols = [:ka1,:kb1,:kcat1,:ka2,:kb2,:ka3,:kb3,:ka4,:kb4,:kcat4,:ka5,:kb5,:kcat5,:ka6,:kb6,:ka7,:kb7,:kcat7,:y]
+pmap = [x[1] => x[2] for x in zip(param_symbols,p)]
+
+u_symbols = [:L,:Lp,:K,:LK,:A,:LpA,:LpAK,:LpAKL,:P,:LpP,:LpAP,:LpAPLp]
+umap = [y[1] => y[2] for y in zip(u_symbols,u0)]
 
 
 rn = @reaction_network begin
-    ka1, L+K --> LK
-    kb1, LK --> L+K
-    kcat1, LK --> Lp+K 
-    ka2, Lp+A --> LpA
-    kb2, LpA --> Lp+A 
-    ka3, LpA+K --> LpAK 
-    kb3, LpAK --> LpA+K 
-    ka4, LpAK+L --> LpAKL
-    kb4, LpAKL --> LpAK+L
-    kcat4, LpAKL --> Lp+LpAK  
-    ka5, Lp+P --> LpP 
-    kb5, LpP --> Lp+P
-    kcat5, LpP --> L+P
-    ka6, LpA+P --> LpAP 
-    kb6, LpAP --> LpA+P
-    ka7, Lp+LpAP --> LpAPLp
-    kb7, LpAPLp --> Lp+LpAP 
-    kcat7, LpAPLp --> L+LpAP 
-end ka1 kb1 kcat1 ka2 kb2 ka3 kb3 ka4 kb4 kcat4 ka5 kb5 kcat5 ka6 kb6 ka7 kb7 kcat7 
+    (ka1,kb1), L + K <--> LK
+    kcat1, LK --> Lp + K 
+    (ka2,kb2), Lp + A <--> LpA 
+    (ka3,kb3), LpA + K <--> LpAK  
+    (ka4*y,kb4), LpAK + L <--> LpAKL
+    kcat4, LpAKL --> Lp + LpAK  
+    (ka5,kb5), Lp + P <--> LpP 
+    kcat5, LpP --> L + P
+    (ka6,kb6), LpA + P <--> LpAP 
+    (ka7*y,kb7), Lp + LpAP <--> LpAPLp
+    kcat7, LpAPLp --> L + LpAP 
+end ka1 kb1 kcat1 ka2 kb2 ka3 kb3 ka4 kb4 kcat4 ka5 kb5 kcat5 ka6 kb6 ka7 kb7 kcat7 y 
 
-oprob = ODEProblem(rn, u0, tspan, p)
+oprob = ODEProblem(rn, umap, tspan, pmap)
 osol = solve(oprob, Tsit5())
-plot(osol)
+plot(osol,linewidth = 1.5, size = (1100,700))
