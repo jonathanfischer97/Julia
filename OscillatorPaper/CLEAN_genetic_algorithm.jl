59ba5b2a51da649c20ee56080268c0380640f9e3
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-03-17 19:08:21 -0400

    doesn't work for some reason, optimized cost func?

--- OscillatorPaper/CLEAN_genetic_algorithm.jl
+++ OscillatorPaper/CLEAN_genetic_algorithm.jl
@@ -2,7 +2,7 @@ using Catalyst
 using DifferentialEquations
 using Plots
 using Evolutionary
-using StaticArrays
+# using StaticArrays
 using BenchmarkTools
 using FFTW
 # using Zygote
@@ -17,6 +17,17 @@ Threads.nthreads()
 
 
 ## COST FUNCTIONS and dependencies 
+function getDif(indexes::Vector{Int}, arrayData::Vector{Float64})::Float64 #get difference between fft peak indexes
+    arrLen = length(indexes)
+    if arrLen < 2
+        return 0.0
+    end
+    sum_diff = @inbounds sum(arrayData[indexes[i]] - arrayData[indexes[i+1]] for i in 1:(arrLen-1))
+    sum_diff += arrayData[indexes[end]]
+    return sum_diff
+end
+
+## COST FUNCTIONS and dependencies 
 function getDif(indexes::Vector{Int}, arrayData::Vector{Float64})::Float64 #get difference between peaks 
     arrLen = length(indexes)
     sum = 0
@@ -29,26 +40,29 @@ function getDif(indexes::Vector{Int}, arrayData::Vector{Float64})::Float64 #get 
     sum += arrayData[indexes[end]] 
     return sum
 end
+
     
 function getSTD(indexes::Vector{Int}, arrayData::Vector{Float64}, window::Int)::Float64 #get standard deviation of fft peak indexes
     numPeaks = length(indexes)
-    arrLen = length(arrayData)
-    sum = 0
+    sum = 0.0
     for ind in indexes 
         minInd = max(1, ind - window)
-        maxInd = min(arrLen, ind+window)
+        maxInd = min(length(arrayData), ind + window)
         sum += std(arrayData[minInd:maxInd])
     end
-    sum = sum/numPeaks 
-    return sum
+    return sum / numPeaks
 end
 
+function getSTD(indexes::Vector{Int}, arrayData::Vector{Float64}, window::Int)::Float64 #get standard deviation of fft peak indexes
+    sum_std = @inbounds sum(std(arrayData[max(1, ind - window):min(length(arrayData), ind + window)]) for ind in indexes)
+    return sum_std / length(indexes)
+end
+
+
  
-function getFrequencies(y::Vector{Float64}) #normally would slice y by interval for a sampling rate but not here
-    #fft sample rate: 1 sample per 5 minutes
-    res = broadcast(abs,rfft(y)) #broadcast abs to all elements of rfft return array. Think .= syntax does the same 
-    #normalize the amplitudes
-    res/cld(10000, 2)
+function getFrequencies(y::Vector{Float64})::Vector{Float64} #get fft of ODE solution
+    res = broadcast(abs,rfft(y)) #broadcast abs to all elements of rfft return array. 
+    res/cld(10000, 2) #normalize the amplitudes
 end
 
 
@@ -81,31 +95,33 @@ osc_rn = @reaction_network osc_rn begin
     kcat7, LpAPLp --> L + LpAP 
 
     #previously hidden NERDSS reactions
-    (ka2,kb2), Lp + AK <--> LpAK
-    (ka2*y,kb2), Lp + AKL <--> LpAKL
-    (ka2,kb2), Lp + AP <--> LpAP
-    (ka2*y,kb2), Lp + APLp <--> LpAPLp
-    (ka3,kb3), A + K <--> AK
-    (ka4,kb4), A + P <--> AP
-    (ka3,kb3), A + LK <--> AKL
-    (ka4,kb4), A + LpP <--> APLp
-    (ka3*y,kb3), LpA + LK <--> LpAKL
-    (ka4*y,kb4), LpA + LpP <--> LpAPLp
-    (ka1,kb1), AK + L <--> AKL #binding of kinase to lipid
-    kcat1, AKL --> Lp + AK #phosphorylation of lipid
-    (ka7,kb7), AP + Lp <--> APLp #binding of phosphatase to lipid
-    kcat7, APLp --> L + AP #dephosphorylation of lipid
+    # (ka2,kb2), Lp + AK <--> LpAK
+    # (ka2*y,kb2), Lp + AKL <--> LpAKL
+    # (ka2,kb2), Lp + AP <--> LpAP
+    # (ka2*y,kb2), Lp + APLp <--> LpAPLp
+    # (ka3,kb3), A + K <--> AK
+    # (ka4,kb4), A + P <--> AP
+    # (ka3,kb3), A + LK <--> AKL
+    # (ka4,kb4), A + LpP <--> APLp
+    # (ka3*y,kb3), LpA + LK <--> LpAKL
+    # (ka4*y,kb4), LpA + LpP <--> LpAPLp
+    # (ka1,kb1), AK + L <--> AKL #binding of kinase to lipid
+    # kcat1, AKL --> Lp + AK #phosphorylation of lipid
+    # (ka7,kb7), AP + Lp <--> APLp #binding of phosphatase to lipid
+    # kcat7, APLp --> L + AP #dephosphorylation of lipid
 end ka1 kb1 kcat1 ka2 kb2 ka3 kb3 ka4 kb4 ka7 kb7 kcat7 y 
 
 input = "{'ka1': 0.01642097502199415, 'kb1': 0.0643048008980449, 'kcat1': 286.6382253193995, 'ka2': 1.0, 'kb2': 0.39569337786534897, 'ka3': 0.024784025572903687, 'kb3': 0.5393197910059361, 'ka4': 0.03281183067630924, 'kb4': 0.2897657637531564, 'ka7': 0.11450246770405478, 'kb7': 0.0028126177315505618, 'kcat7': 1.2733781341040291, 'VA': 2650.5049775102034, 'L': 2.107360215531841, 'Lp': 2.624942515606095, 'K': 0.15746804082956445, 'P': 0.7958264308210892, 'A': 2.785231513223431}"
 parsed = parse_input(input)
 
 p = parsed[1:end-5]
-u0 = [:L => parsed[end-4], :Lp => parsed[end-3], :K => parsed[end-2], :P => parsed[end-1], :A => parsed[end], :LK => 0., :LpA => 0., :LpAK => 0., :LpAP => 0., :LpAPLp => 0., :LpAKL => 0., :LpP => 0., :AK => 0., :AP => 0., :AKL => 0., :APLp => 0.]
+u0 = [:L => parsed[end-4], :Lp => parsed[end-3], :K => parsed[end-2], :P => parsed[end-1], :A => parsed[end], :LK => 0., :LpA => 0., :LpAK => 0., :LpAP => 0., :LpAPLp => 0., :LpAKL => 0., :LpP => 0.]#, :AK => 0., :AP => 0., :AKL => 0., :APLp => 0.]
 
 
 tspan = (0.,100.)
 
+
+u0 = [0.0, 0.3, 0.0, 3.0, 0.9, 0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0]
 oprob = ODEProblem(osc_rn, u0, tspan, p)
 osol = solve(oprob, saveat = 0.01)
 plot(osol)
@@ -137,7 +153,7 @@ param_values = Dict(
     "ka7" => Dict("min" => ka_min, "max" => ka_max),
     "kb7" => Dict("min" => kb_min, "max" => kb_max),
     "kcat7" => Dict("min" => kcat_min, "max" => kcat_max),
-    "y" => Dict("min" => 100., "max" => 10000.)
+    "y" => Dict("min" => 100., "max" => 5000.)
 )
 
 
@@ -180,14 +196,12 @@ test_fitness_function = make_test_fitness_function(oprob, target_ss)
 
 
 constraints = BoxConstraints([param_values[p]["min"] for p in keys(param_values)], [param_values[p]["max"] for p in keys(param_values)])
-opts = Evolutionary.Options(show_trace=true,show_every=1, store_trace=true, iterations=20, parallelization=:thread)
+opts = Evolutionary.Options(show_trace=true,show_every=1, store_trace=true, iterations=10, parallelization=:serial)
 
 common_range = 0.5
 valrange = fill(common_range, 13)
 
-mthd = GA(populationSize = 5000, selection = tournament(500;select=argmax),
-crossover = TPX, crossoverRate = 0.5,
-mutation  = BGA(valrange, 2), mutationRate = 0.75)
+mthd = GA(populationSize = 5000, selection = tournament(500;select=argmax), crossover = TPX, crossoverRate = 0.5, mutation  = BGA(valrange, 2), mutationRate = 0.75)
 
 result = Evolutionary.optimize(test_fitness_function, constraints, mthd, opts)
 
@@ -201,9 +215,9 @@ newsol[1, end] # Get the steady-state values of the ODEProblem solution
 
 
 ## REAL FITNESS FUNCTION ##
-function eval_fitness(p::Vector{Float64}, prob::ODEProblem)
+function eval_fitness(p::Vector{Float64}, prob::ODEProblem)::Float64
     Y = solve(remake(prob, p=p), saveat = 0.01)
-    p1 = Y[1,:]
+    p1 = Y[1,:] #get first species
     fftData = getFrequencies(p1) #get Fourier transform of p1
     indexes = findmaxima(fftData)[1] #get indexes of local maxima of fft
     if length(indexes) == 0 #if no peaks, return 0
@@ -232,10 +246,10 @@ opts = Evolutionary.Options(show_trace=true,show_every=1, store_trace=true, iter
 
 common_range = 0.7
 valrange = fill(common_range, 13)
-mthd = GA(populationSize = 10000, selection = tournament(1000;select=argmax),
+mthd = GA(populationSize = 1000, selection = tournament(100;select=argmax),
           crossover = TPX, crossoverRate = 0.5,
           mutation  = BGA(valrange, 2), mutationRate = 0.75)
-result = Evolutionary.optimize(fitness_function, p, mthd, opts)
+result = Evolutionary.optimize(fitness_function, constraints, mthd, opts)
 
 
 
