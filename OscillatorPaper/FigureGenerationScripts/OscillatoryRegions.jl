6a6439c0cac600008874477117ab5f53833e912d
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-05-27 01:35:07 -0400

    got scatter plot finally working

--- OscillatorPaper/FigureGenerationScripts/OscillatoryRegions.jl
+++ OscillatorPaper/FigureGenerationScripts/OscillatoryRegions.jl
@@ -75,11 +75,11 @@ end
 
 
 function get_range(paramrange::Symbol, constraints::ParameterConstraints, length::Int)
-    return exp10.(range(constraints.data[paramrange].min, constraints.data[paramrange].max, length=length))
+    return exp10.(range(getproperty(constraints,paramrange).min, getproperty(constraints,paramrange).max, length=length))
 end
 
 function get_range(ICrange::Symbol, constraints::InitialConditionConstraints, length::Int)
-    return range(constraints.data[ICrange].min, constraints.data[ICrange].max, length=length)
+    return range(getproperty(constraints,ICrange).min, getproperty(constraints,ICrange).max, length=length)
 end
 
 
@@ -87,8 +87,8 @@ end
 #! Define the function to evaluate the 2D solution space for oscillations
 function evaluate_2D_solution_space(paramrange_pair::Vector{Symbol}, prob::ODEProblem, constraints::ConstraintType; steps=300)
     # Get string names from pair
-    p1name = getproperty(constraints.data, paramrange_pair[1]).name
-    p2name = getproperty(constraints.data, paramrange_pair[2]).name
+    p1name = getproperty(constraints, paramrange_pair[1]).name
+    p2name = getproperty(constraints, paramrange_pair[2]).name
 
     constraints isa ParameterConstraints ? NAMES = PARAM_NAMES : NAMES = VAR_NAMES #TODO Make this generic
 
@@ -97,9 +97,7 @@ function evaluate_2D_solution_space(paramrange_pair::Vector{Symbol}, prob::ODEPr
     
     # Get evaluation function
     #TODO Make this generic, too much type checking
-    constraints isa ParameterConstraints ? evalfunc = eval_param_fitness : evalfunc = eval_ic_fitness
-
-    evalfunc_closed(newparams) = evalfunc(newparams, prob)
+    evalfunc = constraints isa ParameterConstraints ? eval_param_fitness : eval_ic_fitness
 
     
     # Get ranges of parameters
@@ -122,9 +120,9 @@ function evaluate_2D_solution_space(paramrange_pair::Vector{Symbol}, prob::ODEPr
         newparams = copy(prob.u0) #TODO Make this generic
         for j in 1:p2_length
             update_params!((p1_range[i], p2_range[j]), newparams, p1idx, p2idx)
-            results = evalfunc_closed(newparams)
+            results = evalfunc(newparams,prob)
             oscillation_scores[j, i] = results[1]
-            periods[j, i] = results[1] < -0.1 ? results[2] : 0.0
+            periods[j, i] = results[1] < -0.2 ? results[2] : 0.0
             next!(innerprogress)
         end
     end
@@ -151,11 +149,6 @@ function plot_oscillation_contour(result; xscaling=:log10, yscaling=:log10)
     var1_range = result.ranges[1]
     var2_range = result.ranges[2]
 
-    # xscaling = xscaling
-    # if "PIP/PIP2" in result.names
-    #     xscaling = :log10
-    # end
-
     oscplot = contour(var1_range, var2_range, result.fit, xscale=xscaling, yscale=yscaling, 
                         title="Oscillation Scores", xlabel=unit_labeler(result.names[1]), ylabel=unit_labeler(result.names[2]), colorbar_title="Oscillation Index", color=:vik, bottom_margin = 12px, left_margin = 16px, top_margin = 8px)
 
@@ -165,14 +158,91 @@ function plot_oscillation_contour(result; xscaling=:log10, yscaling=:log10)
     plot(oscplot, freqplot, layout=(2,1), size=(1000, 1000))
 end
 
+function plot_oscillation_heatmap(result; xscaling=:log10, yscaling=:log10)
+    var1_range = collect(result.ranges[1])
+    var2_range = collect(result.ranges[2])
 
+    oscplot = heatmap(var1_range, var2_range, result.fit, xscale=xscaling, yscale=yscaling,
+                        title="Oscillation Scores", xlabel=unit_labeler(result.names[1]), ylabel=unit_labeler(result.names[2]), colorbar_title="Oscillation Index", color=:vik, bottom_margin = 12px, left_margin = 16px, top_margin = 8px)
+
+    frequencies = ifelse.(result.per .== 0, 0, 1 ./ result.per)
+    freqplot = heatmap(var1_range, var2_range, frequencies, xscale=xscaling, yscale=yscaling,
+                        title="Frequencies", xlabel=unit_labeler(result.names[1]), ylabel=unit_labeler(result.names[2]), colorbar_title="Frequency (Hz)", color=:plasma, bottom_margin = 12px, left_margin = 16px, top_margin = 8px)
+    plot(oscplot, freqplot, layout=(2,1), size=(1000, 1000))
+end
+
+function plot_oscillation_surface(result; xscaling=:log10, yscaling=:log10)
+    plotlyjs()
+    var1_range = collect(result.ranges[1])
+    var2_range = collect(result.ranges[2])
+
+    oscplot = surface(var1_range, var2_range, result.fit, xscale=xscaling, yscale=yscaling,
+                        title="Oscillation Scores", xlabel=unit_labeler(result.names[1]), ylabel=unit_labeler(result.names[2]), colorbar_title="Oscillation Index", color=:vik, bottom_margin = 12px, left_margin = 16px, top_margin = 8px)
+
+    frequencies = ifelse.(result.per .== 0, 0, 1 ./ result.per)
+    freqplot = surface(var1_range, var2_range, frequencies, xscale=xscaling, yscale=yscaling,
+                        title="Frequencies", xlabel=unit_labeler(result.names[1]), ylabel=unit_labeler(result.names[2]), colorbar_title="Frequency (Hz)", color=:plasma, bottom_margin = 12px, left_margin = 16px, top_margin = 8px)
+    plot(oscplot, freqplot, layout=(2,1), size=(1000, 1000))
+    gr()
+end
+
+
+function plot_oscillation_scatter(result; xscaling=:log10, yscaling=:log10, step = 5)
+    var1_range = result.ranges[1]
+    sliced_var1_range = var1_range[1:step:end]
+
+    var2_range = result.ranges[2]
+    sliced_var2_range = var2_range[1:step:end]
+
+    fitness_array = result.fit
+    sliced_fitness_array = fitness_array[1:step:end,1:step:end]
+
+    frequency_array = ifelse.(result.per .== 0, 0, 1 ./ result.per)
+    sliced_frequency_array = frequency_array[1:step:end,1:step:end]
+
+
+    # Initialize the plot with the first column or row
+    oscplot = scatter(xlims=(0,var1_range[end]),ylims=(0,var2_range[end]), zlims = (minimum(sliced_fitness_array), maximum(sliced_fitness_array)), xscale=xscaling, yscale=yscaling,
+                        title="Oscillation Scores", xlabel=unit_labeler(result.names[1]), ylabel=unit_labeler(result.names[2]), colorbar_title="Oscillation Index", label=false)
+
+    # Do the same for the frequencies plot
+    freqplot = scatter(xlims=(0,var1_range[end]),ylims=(0,var2_range[end]), zlims = (minimum(sliced_frequency_array), maximum(sliced_frequency_array)), xscale=xscaling, yscale=yscaling,
+                        title="Frequencies", xlabel=unit_labeler(result.names[1]), ylabel=unit_labeler(result.names[2]), colorbar_title="Frequency (Hz)")
+
+
+    # # Add the rest of the columns or rows iteratively
+    # for j in 1+step:step:length(var2_range)
+    #     scatter!(oscplot, sliced_var1_range, fill(var2_range[j],length(sliced_var1_range)), marker_z = fitness_array[j,1:step:end], color = :vik, label=false)
+    # end
+
+    for (i,var1) in enumerate(sliced_var1_range)
+        for (j,var2) in enumerate(sliced_var2_range)
+            if sliced_fitness_array[j,i] < 0.2
+                scatter!(oscplot, [var1], [var2], color = :white, label=false, markeralpha = 0.0)
+                scatter!(freqplot, [var1], [var2], color = :white, label=false, markeralpha = 0.0)
+            else
+                scatter!(oscplot, [var1], [var2], marker_z = [sliced_fitness_array[j,i]], color = :amp, label=false)
+                scatter!(freqplot, [var1], [var2], marker_z = [sliced_frequency_array[j,i]], color = :lighttest, label=false)
+            end
+        end
+    end
+
+    # Combine both plots
+    plot(oscplot, freqplot, layout=(2,1), size=(1000, 1000), bottom_margin = 12px, left_margin = 16px, top_margin = 8px)
+end
+
+
+
+icranges = define_initialcondition_constraints()
 
 testresult = evaluate_2D_solution_space([:L, :A], fullprob, icranges; steps =300)
-testpers = testresult.per
 
 #* Plot oscillatory regions of 2D parameter space with contour or heatplot
-testplot = plot_oscillation_contour(testresult; xscaling=:identity, yscaling=:identity)
-savefig(testplot, joinpath(@__DIR__, "testplot_ka2_DF.png"))
+testcontour = plot_oscillation_contour(testresult; xscaling=:identity, yscaling=:identity)
+testscatter = plot_oscillation_heatmap(testresult; xscaling=:identity, yscaling=:identity)
+testsurface = plot_oscillation_surface(testresult; xscaling=:identity, yscaling=:identity)
+testscatter = plot_oscillation_scatter(testresult; xscaling=:identity, yscaling=:identity)
+savefig(testscatter, joinpath(@__DIR__, "testscatter_PIP_AP2.png"))
 
 
 
