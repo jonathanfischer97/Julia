fa9c8bcd72c0cce71e0fb008d9f3c99ca325781c
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-05-27 01:34:52 -0400

    can't seem to speed up cost function any more

---
+++ SCRATCHPAD.jl
@@ -0,0 +1,34 @@
+using BenchmarkTools, Profile
+
+testarray = rand(Float64, 1000000)
+testpeakidxs, testpeakvals = findmaxima(testarray, 1)
+
+@btime @fastmath getDif($testpeakidxs, $testarray)
+
+
+#< Benchmarking automatic vs stiff solvers
+@benchmark autosol = solve(fullprob, saveat=0.1, save_idxs=1, maxiters=10000, verbose=false)
+@benchmark @fastmath solve(fullprob, saveat=0.1, save_idxs=1, maxiters=10000, verbose=false)
+plot(autosol)
+@benchmark stiffsol = solve(fullprob, Rosenbrock23(), saveat=0.1, save_idxs=1, maxiters=10000, verbose=false)
+plot!(stiffsol)
+
+difference = sum(autosol - stiffsol)
+
+
+
+@btime CostFunction($autosol)
+@btime @fastmath CostFunction($autosol)
+
+
+#< PerAmp testing 
+per, amp = getPerAmp(autosol)
+
+
+eval_ic_fitness(fullprob.u0, fullprob)
+
+
+#< Comparing getDif_bidirectional to getDif
+getDif(testpeakidxs, testarray)
+getDif(testpeakvals)
+getDif_bidirectional(testpeakvals)
