86532e8318a9761ae81fe5004f6176b6b3f20ced
jonathanfischer97 <jfisch27@jhu.edu>
2023-05-29 16:20:40 -0400

    commented out other rn

--- UTILITIES/ReactionNetwork.jl
+++ UTILITIES/ReactionNetwork.jl
@@ -1,5 +1,5 @@
-const PARAM_NAMES = ["ka1", "kb1", "kcat1", "ka2", "kb2", "ka3", "kb3", "ka4", "kb4", "ka7", "kb7", "kcat7", "DF"]
-const VAR_NAMES = ["L", "K", "P", "A", "Lp", "LpA", "LK", "LpP", "LpAK", "LpAP", "LpAKL", "LpAPLp", "AK", "AP", "AKL", "APLp"]
+# const PARAM_NAMES = ["ka1", "kb1", "kcat1", "ka2", "kb2", "ka3", "kb3", "ka4", "kb4", "ka7", "kb7", "kcat7", "DF"]
+# const VAR_NAMES = ["L", "K", "P", "A", "Lp", "LpA", "LK", "LpP", "LpAK", "LpAP", "LpAKL", "LpAPLp", "AK", "AP", "AKL", "APLp"]
 
 """Full oscillator model"""
 fullrn = @reaction_network fullrn begin
@@ -37,21 +37,21 @@ fullrn = @reaction_network fullrn begin
 end  
 
 
-"""Original oscillator model, without all possible pairs of reactions"""
-originalrn = @reaction_network originalrn begin
-    @parameters ka1 kb1 kcat1 ka2 kb2 ka3 kb3 ka4 kb4 ka7 kb7 kcat7 DF
-    @species L(t) K(t) P(t) A(t) Lp(t) LpA(t) LK(t) LpP(t) LpAK(t) LpAP(t) LpAKL(t) LpAPLp(t) 
-    # ALIASES: L = PIP, Lp = PIP2, K = Kinase, P = Phosphatase, A = AP2 
-    # reactions between the same binding interfaces will have the same rate constant no matter the dimensionality or complex
-    (ka1,kb1), L + K <--> LK # L binding to kinase
-    kcat1, LK --> Lp + K # L phosphorylation by kinase into Lp
-    (ka2,kb2), Lp + A <--> LpA # Lp binding to AP2 adaptor
-    (ka3,kb3), LpA + K <--> LpAK # Membrane-bound adaptor binding to kinase
-    (ka1*DF,kb1), LpAK + L <--> LpAKL # 2D reaction: Membrane-bound kinase binds to L with greater affinity as determined by y (V/A)
-    kcat1, LpAKL --> Lp + LpAK # L phosphorylation by kinase into Lp, same as 3D: first order reactions aren't dependent on dimensionality 
-    (ka7,kb7), Lp + P <--> LpP # Lp binding to phosphatase
-    kcat7, LpP --> L + P # L dephosphorylation by phosphatase
-    (ka4,kb4), LpA + P <--> LpAP # Membrane-bound adaptor binding to phosphatase 
-    (ka7*DF,kb7), Lp + LpAP <--> LpAPLp # 2D reaction: Membrane-bound phosphatase binds to Lp with greater affinity as determined by y (V/A)
-    kcat7, LpAPLp --> L + LpAP # L dephosphorylation by phosphatase, same as 3D: first order reactions aren't dependent on dimensionality
-end  
+# """Original oscillator model, without all possible pairs of reactions"""
+# originalrn = @reaction_network originalrn begin
+#     @parameters ka1 kb1 kcat1 ka2 kb2 ka3 kb3 ka4 kb4 ka7 kb7 kcat7 DF
+#     @species L(t) K(t) P(t) A(t) Lp(t) LpA(t) LK(t) LpP(t) LpAK(t) LpAP(t) LpAKL(t) LpAPLp(t) 
+#     # ALIASES: L = PIP, Lp = PIP2, K = Kinase, P = Phosphatase, A = AP2 
+#     # reactions between the same binding interfaces will have the same rate constant no matter the dimensionality or complex
+#     (ka1,kb1), L + K <--> LK # L binding to kinase
+#     kcat1, LK --> Lp + K # L phosphorylation by kinase into Lp
+#     (ka2,kb2), Lp + A <--> LpA # Lp binding to AP2 adaptor
+#     (ka3,kb3), LpA + K <--> LpAK # Membrane-bound adaptor binding to kinase
+#     (ka1*DF,kb1), LpAK + L <--> LpAKL # 2D reaction: Membrane-bound kinase binds to L with greater affinity as determined by y (V/A)
+#     kcat1, LpAKL --> Lp + LpAK # L phosphorylation by kinase into Lp, same as 3D: first order reactions aren't dependent on dimensionality 
+#     (ka7,kb7), Lp + P <--> LpP # Lp binding to phosphatase
+#     kcat7, LpP --> L + P # L dephosphorylation by phosphatase
+#     (ka4,kb4), LpA + P <--> LpAP # Membrane-bound adaptor binding to phosphatase 
+#     (ka7*DF,kb7), Lp + LpAP <--> LpAPLp # 2D reaction: Membrane-bound phosphatase binds to Lp with greater affinity as determined by y (V/A)
+#     kcat7, LpAPLp --> L + LpAP # L dephosphorylation by phosphatase, same as 3D: first order reactions aren't dependent on dimensionality
+# end  
