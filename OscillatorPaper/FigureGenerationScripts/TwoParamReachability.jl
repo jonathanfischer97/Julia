edad1c7c93bd3deac0a8c62d1699674d5b2a2c41
jonathanfischer97 <jfisch27@jhu.edu>
2023-06-06 14:13:51 -0400

    put odeprob into function, still type unstable

--- OscillatorPaper/FigureGenerationScripts/TwoParamReachability.jl
+++ OscillatorPaper/FigureGenerationScripts/TwoParamReachability.jl
@@ -40,32 +40,33 @@ end
 
 
 #! Solve model for arbitrary oscillatory parameters and initial conditions
-begin
+function make_ODEProb()#{Vector{Float64}}{Tuple{Float64, Float64}}
     #? Parameter list
-    psym = [:ka1 => 0.009433439939827041, :kb1 => 2.3550169939427845, :kcat1 => 832.7213093872278, :ka2 => 12.993995997539924, :kb2 => 6.150972501791291,
-            :ka3 => 1.3481451097940793, :kb3 => 0.006201726090609513, :ka4 => 0.006277294665474662, :kb4 => 0.9250191811994848, :ka7 => 57.36471615394549, 
-            :kb7 => 0.04411989797898752, :kcat7 => 42.288085868394326, :DF => 3631.050539219606]
-    p = [x[2] for x in psym]
-        
+    # psym = [:ka1 => 0.009433439939827041, :kb1 => 2.3550169939427845, :kcat1 => 832.7213093872278, :ka2 => 12.993995997539924, :kb2 => 6.150972501791291,
+    #         :ka3 => 1.3481451097940793, :kb3 => 0.006201726090609513, :ka4 => 0.006277294665474662, :kb4 => 0.9250191811994848, :ka7 => 57.36471615394549, 
+    #         :kb7 => 0.04411989797898752, :kcat7 => 42.288085868394326, :DF => 3631.050539219606]
+    # p::Vector{Float64} = [x[2] for x in psym]
+    p = [0.009433439939827041, 2.3550169939427845, 832.7213093872278, 12.993995997539924, 6.150972501791291, 
+            1.3481451097940793, 0.006201726090609513, 0.006277294665474662, 0.9250191811994848, 57.36471615394549, 
+            0.04411989797898752, 42.288085868394326, 3631.050539219606]    
     #? Initial condition list
-    usym = [:L => 0.0, :K => 0.5, :P => 0.3, :A => 2.0, :Lp => 3.0, :LpA => 0.0, :LK => 0.0, 
-            :LpP => 0.0, :LpAK => 0.0, :LpAP => 0.0, :LpAKL => 0.0, :LpAPLp => 0.0, :AK => 0.0, :AP => 0.0, 
-            :AKL => 0.0, :APLp => 0.0]
-    u0 = [x[2] for x in usym]
-
+    # usym = [:L => 0.0, :K => 0.5, :P => 0.3, :A => 2.0, :Lp => 3.0, :LpA => 0.0, :LK => 0.0, 
+    #         :LpP => 0.0, :LpAK => 0.0, :LpAP => 0.0, :LpAKL => 0.0, :LpAPLp => 0.0, :AK => 0.0, :AP => 0.0, 
+    #         :AKL => 0.0, :APLp => 0.0]
+    # u0::Vector{Float64} = [x[2] for x in usym]
+    u0 = [0.0, 0.5, 0.3, 2.0, 3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     #? Timespan for integration
     tspan = (0., 100.)
 
     #? Create ODE problem and solve
-    fullprob = ODEProblem(fullrn, u0, tspan, p) #TODO fix type stability
-    sol = solve(fullprob, saveat=0.1, save_idxs=1) #TODO fix type stability
+    return ODEProblem(fullrn, u0, tspan, p) #TODO fix type stability
+    # sol = solve(fullprob, saveat=0.1, save_idxs=1) #TODO fix type stability
 
     #? Plot the results
     # plot(sol)
 end
-# Profile.print()
-# @benchmark solve($fullprob, saveat=0.1, save_idxs=1)
-# @benchmark solve($odeprob, saveat=0.1, save_idxs=1)
+@code_warntype make_ODEProb()
+fullprob = make_ODEProb()
 
 """Returns the function factory for the cost function, referencing the ODE problem, tracker, and fixed inputs with closure"""
 function make_fitness_function_with_fixed_inputs(evalfunc::Function, prob::ODEProblem, fixed_input_pair::Vector{ConstraintRange}, pair_idxs::Tuple{Int,Int})
@@ -95,10 +96,10 @@ end
 #TODO Testing type stability
 fitness_function = make_fitness_function_with_fixed_inputs(eval_ic_fitness, fullprob, ic_constraints.ranges[1:2], (1, 2))
 @code_warntype fitness_function(rand(14))
-@benchmark fitness_function($(u0[1:end-2]))
+@code_warntype fitness_function(u0[1:end-2])
 
 @benchmark solve($fullprob, saveat=0.1, save_idxs=1)
-@code_warntype ODEProblem(fullrn, usym, tspan, psym)
+@code_warntype ODEProblem(fullrn, u0, tspan, p)
 @code_warntype solve(fullprob, saveat=0.1, save_idxs=1)
 @code_warntype eval_ic_fitness(rand(14), fullprob)
 
