fc472e3408b77ea37320f0508bca237ef070a799
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-06-06 21:23:00 -0400

    made into function

--- UTILITIES/ReactionNetwork.jl
+++ UTILITIES/ReactionNetwork.jl
@@ -2,39 +2,43 @@
 # const VAR_NAMES = ["L", "K", "P", "A", "Lp", "LpA", "LK", "LpP", "LpAK", "LpAP", "LpAKL", "LpAPLp", "AK", "AP", "AKL", "APLp"]
 
 """Full oscillator model"""
-fullrn = @reaction_network fullrn begin
-    @parameters ka1 kb1 kcat1 ka2 kb2 ka3 kb3 ka4 kb4 ka7 kb7 kcat7 DF
-    @species L(t) K(t) P(t) A(t) Lp(t) LpA(t) LK(t) LpP(t) LpAK(t) LpAP(t) LpAKL(t) LpAPLp(t) AK(t) AP(t) AKL(t) APLp(t)
-    # ALIASES: L = PIP, Lp = PIP2, K = Kinase, P = Phosphatase, A = AP2 
-    # reactions between the same binding interfaces will have the same rate constant no matter the dimensionality or complex
-    (ka1,kb1), L + K <--> LK # L binding to kinase
-    kcat1, LK --> Lp + K # L phosphorylation by kinase into Lp
-    (ka2,kb2), Lp + A <--> LpA # Lp binding to AP2 adaptor #*POSSIBLY FIXED
-    (ka3,kb3), LpA + K <--> LpAK # Membrane-bound adaptor binding to kinase
-    (ka1*DF,kb1), LpAK + L <--> LpAKL # 2D reaction: Membrane-bound kinase binds to L with greater affinity as determined by y (V/A)
-    kcat1, LpAKL --> Lp + LpAK # L phosphorylation by kinase into Lp, same as 3D: first order reactions aren't dependent on dimensionality 
-    (ka7,kb7), Lp + P <--> LpP # Lp binding to phosphatase #*POSSIBLY FIXED
-    kcat7, LpP --> L + P # L dephosphorylation by phosphatase
-    (ka4,kb4), LpA + P <--> LpAP # Membrane-bound adaptor binding to phosphatase 
-    (ka7*DF,kb7), Lp + LpAP <--> LpAPLp # 2D reaction: Membrane-bound phosphatase binds to Lp with greater affinity as determined by y (V/A)
-    kcat7, LpAPLp --> L + LpAP # L dephosphorylation by phosphatase, same as 3D: first order reactions aren't dependent on dimensionality
+function make_fullrn()
+    fullrn = @reaction_network fullrn begin
+        @parameters ka1 kb1 kcat1 ka2 kb2 ka3 kb3 ka4 kb4 ka7 kb7 kcat7 DF
+        @species L(t) K(t) P(t) A(t) Lp(t) LpA(t) LK(t) LpP(t) LpAK(t) LpAP(t) LpAKL(t) LpAPLp(t) AK(t) AP(t) AKL(t) APLp(t)
+        # ALIASES: L = PIP, Lp = PIP2, K = Kinase, P = Phosphatase, A = AP2 
+        # reactions between the same binding interfaces will have the same rate constant no matter the dimensionality or complex
+        (ka1,kb1), L + K <--> LK # L binding to kinase
+        kcat1, LK --> Lp + K # L phosphorylation by kinase into Lp
+        (ka2,kb2), Lp + A <--> LpA # Lp binding to AP2 adaptor #*POSSIBLY FIXED
+        (ka3,kb3), LpA + K <--> LpAK # Membrane-bound adaptor binding to kinase
+        (ka1*DF,kb1), LpAK + L <--> LpAKL # 2D reaction: Membrane-bound kinase binds to L with greater affinity as determined by y (V/A)
+        kcat1, LpAKL --> Lp + LpAK # L phosphorylation by kinase into Lp, same as 3D: first order reactions aren't dependent on dimensionality 
+        (ka7,kb7), Lp + P <--> LpP # Lp binding to phosphatase #*POSSIBLY FIXED
+        kcat7, LpP --> L + P # L dephosphorylation by phosphatase
+        (ka4,kb4), LpA + P <--> LpAP # Membrane-bound adaptor binding to phosphatase 
+        (ka7*DF,kb7), Lp + LpAP <--> LpAPLp # 2D reaction: Membrane-bound phosphatase binds to Lp with greater affinity as determined by y (V/A)
+        kcat7, LpAPLp --> L + LpAP # L dephosphorylation by phosphatase, same as 3D: first order reactions aren't dependent on dimensionality
 
-    #previously excluded reactions, all possible combinations possible in vitro
-    (ka2,kb2), Lp + AK <--> LpAK
-    (ka2*DF,kb2), Lp + AKL <--> LpAKL
-    (ka2,kb2), Lp + AP <--> LpAP
-    (ka2*DF,kb2), Lp + APLp <--> LpAPLp
-    (ka3,kb3), A + K <--> AK
-    (ka4,kb4), A + P <--> AP
-    (ka3,kb3), A + LK <--> AKL
-    (ka4,kb4), A + LpP <--> APLp
-    (ka3*DF,kb3), LpA + LK <--> LpAKL
-    (ka4*DF,kb4), LpA + LpP <--> LpAPLp
-    (ka1,kb1), AK + L <--> AKL #binding of kinase to lipid
-    kcat1, AKL --> Lp + AK #phosphorylation of lipid
-    (ka7,kb7), AP + Lp <--> APLp #binding of phosphatase to lipid
-    kcat7, APLp --> L + AP #dephosphorylation of lipid
-end  
+        #previously excluded reactions, all possible combinations possible in vitro
+        (ka2,kb2), Lp + AK <--> LpAK
+        (ka2*DF,kb2), Lp + AKL <--> LpAKL
+        (ka2,kb2), Lp + AP <--> LpAP
+        (ka2*DF,kb2), Lp + APLp <--> LpAPLp
+        (ka3,kb3), A + K <--> AK
+        (ka4,kb4), A + P <--> AP
+        (ka3,kb3), A + LK <--> AKL
+        (ka4,kb4), A + LpP <--> APLp
+        (ka3*DF,kb3), LpA + LK <--> LpAKL
+        (ka4*DF,kb4), LpA + LpP <--> LpAPLp
+        (ka1,kb1), AK + L <--> AKL #binding of kinase to lipid
+        kcat1, AKL --> Lp + AK #phosphorylation of lipid
+        (ka7,kb7), AP + Lp <--> APLp #binding of phosphatase to lipid
+        kcat7, APLp --> L + AP #dephosphorylation of lipid
+    end  
+    return fullrn
+end
+
 
 
 # """Original oscillator model, without all possible pairs of reactions"""
