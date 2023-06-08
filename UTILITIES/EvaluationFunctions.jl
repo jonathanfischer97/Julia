ce902fdb7537780263af24651e76156e7298a8cc
jonathanfischer97 <jfisch27@jhu.edu>
2023-06-08 09:07:55 -0400

    new dispatch for getPerAmp

--- UTILITIES/EvaluationFunctions.jl
+++ UTILITIES/EvaluationFunctions.jl
@@ -54,7 +54,22 @@ function getPerAmp(sol::ODESolution, indx_max::Vector{Int}, vals_max::Vector{Flo
     # @inbounds amps = 0.5 .* (vals_max .- vals_min)[1:min(length(indx_max), length(indx_min))]
 
     return mean(pers), mean(amps)
+end
 
+function getPerAmp(sol::ODESolution)
+    #* Find peaks of the minima too 
+    indx_max, vals_max = findmaxima(sol[1,:], 5)
+    indx_min, vals_min = findminima(sol[1,:], 5)
+
+    if length(indx_max) < 3 || length(indx_min) < 3
+        return 0.0, 0.0
+    end
+    #* Calculate amplitudes and periods
+    @inbounds pers = (sol.t[indx_max[i+1]] - sol.t[indx_max[i]] for i in 1:(length(indx_max)-1))
+    @inbounds amps = ((vals_max[i] - vals_min[i])/2 for i in 1:min(length(indx_max), length(indx_min)))
+    # @inbounds amps = 0.5 .* (vals_max .- vals_min)[1:min(length(indx_max), length(indx_min))]
+
+    return mean(pers), mean(amps)
 end
 
 """Cost function to be plugged into eval_fitness wrapper"""
