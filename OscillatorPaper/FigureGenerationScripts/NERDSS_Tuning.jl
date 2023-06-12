4b2caa80e76626c75c72fced270f1074d208fae1
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-06-12 13:49:44 -0400

    interesting relationship btw df, AP2, freq

--- OscillatorPaper/FigureGenerationScripts/NERDSS_Tuning.jl
+++ OscillatorPaper/FigureGenerationScripts/NERDSS_Tuning.jl
@@ -1,5 +1,5 @@
 begin 
-    using Plots 
+    using Plots; theme(:juno)
     using Catalyst
     using DifferentialEquations
     using Statistics
@@ -9,7 +9,7 @@ begin
     using Distributions
     using DataFrames
     # using Unitful
-    # using Unitful: µM, nm, s
+    # using Unitful: µM, M, nm, µm, s, μs, Na, L, 𝐍
     using StaticArrays
     using BenchmarkTools, Profile, ProgressMeter
     # using Cthulhu
@@ -26,7 +26,7 @@ begin
     # plotlyjs()
     # import CairoMakie as cm 
     # gr()
-
+    
 
     include("../../UTILITIES/EvolutionaryOverloads.jl")
 
@@ -41,72 +41,289 @@ begin
 
     include("../../UTILITIES/ODEProbMaker.jl")
 
+    include("../../UTILITIES/UnitTools.jl")
+
 
     const SHOW_PROGRESS_BARS = parse(Bool, get(ENV, "PROGRESS_BARS", "true"))
 end
 
 
-
-# format_equations(fullrn)
-
-psym = [:ka1 => 5.453534109021441e-05 #ka1, 1
+psym = [:ka1 => convert_to_macrorate(0.027267670545107203)#5.453534109021441e-05 #ka1, 1
     :kb1 => 0.0643048008980449 #kb1, 2
     :kcat1 => 286.6382253193995 #kcat1, 3
-    :ka2 => 0.0033210781343476934 #ka2, 4
+    :ka2 => convert_to_macrorate(1.6605390671738467) #0.0033210781343476934 #ka2, 4
     :kb2 => 0.39569337786534897 #kb2, 5
-    :ka3 => 8.23096854112845e-05 #ka3, 6
+    :ka3 => convert_to_macrorate(0.04115484270564225) #ka3, 6
     :kb3 => 0.5393197910059361 #kb3, 7
-    :ka4 => 0.0001089706534070095 #ka4, 8
+    :ka4 => convert_to_macrorate(0.05448532670350475) #ka4, 8
     :kb4 => 0.2897657637531564 #kb4, 9
-    :ka7 => 0.0003802716418207893 #ka7, 10
+    :ka7 => convert_to_macrorate(0.19013582091039463) #ka7, 10
     :kb7 => 0.0028126177315505618 #kb7, 11
     :kcat7 => 1.2733781341040291 #kcat7, 12
-    :DF => 2000.] #DF, 13
+    :DF => 25500.] #DF, 13
 p = [x[2] for x in psym]
 
 
 # usym = [:L => 634.6315289074139, :K => 47.42150049582334, :P => 239.66312964177104,  :A => 838.7724702072363, :Lp => 0.0, :LpA => 0.0, :LK => 0.0, #:Lp => 790.5014385747756,
 #         :LpP => 0.0, :LpAK => 0.0, :LpAP => 0.0, :LpAKL => 0.0, :LpAPLp => 0.0, :AK => 0.0, :AP => 0.0, :AKL => 0.0, :APLp => 0.0]
-# u0 = [x[2] for x in usym]
+usym = [:L => 2.0, :K => 0.05, :P => 0.55,  :A => 2.0, :Lp => 0.0, :LpA => 0.0, :LK => 0.0, #:Lp => 790.5014385747756,
+        :LpP => 0.0, :LpAK => 0.0, :LpAP => 0.0, :LpAKL => 0.0, :LpAPLp => 0.0, :AK => 0.0, :AP => 0.0, :AKL => 0.0, :APLp => 0.0]
+u0 = [x[2] for x in usym]
 
 
+nerdssprob = make_ODEProb(psym, usym; tspan = (0., 10000.))
+# nerdssprob = remake(nerdssprob, u0 = 3.0*[x[2] for x in usym])
+nerdsol = solve(nerdssprob, Rosenbrock23(), save_idxs = 1)
+getPerAmp(nerdsol)
+plot(nerdsol)
 
-nerdssprob = make_ODEProb(; psym = psym, tspan = (0., 100.))
+function getFreqvsDF(nerdssprob, DFs=1000:100:1000000)
+    prob = deepcopy(nerdssprob)
+    freqs = Vector{Float64}(undef,length(collect(DFs)))
+    amps = Vector{Float64}(undef,length(collect(DFs)))
+    for (i,DF) in enumerate(DFs)
+        prob.p[13] = DF
+        nerdsol = solve(prob, save_idxs = 1)
+        fit, period, amplitude = CostFunction(nerdsol)
+        
+        if fit == 0.0
+            freqs[i], amps[i] = 0.0, 0.0
+        else 
+            freqs[i], amps[i] = 1/period, amplitude
+        end
+    end
+    return collect(DFs), freqs, amps
+end
 
-nerdsol = solve(nerdssprob, Rosenbrock23(), saveat = 0.1)
-
-
-
-function concentration_to_copy_number(concentration::Float64, volume_um3::Float64)
-    # Convert volume from um^3 to L
-    volume_L = volume_um3 * 1e-15
-    # Calculate the number of moles in the solution
-    moles = concentration * volume_L
-    # Convert moles to molecules (i.e., copy number) using Avogadro's number
-    copy_number = moles * 6.022e23
-    return copy_number
+function getFreqvsDF_threaded(nerdssprob, DFs=1000:100:1000000)
+    freqs = Vector{Float64}(undef,length(collect(DFs)))
+    amps = Vector{Float64}(undef,length(collect(DFs)))
+    Threads.@threads for i in eachindex(DFs)
+        pcopy = deepcopy(nerdssprob.p)
+        pcopy[13] = DFs[i]
+        newprob = remake(nerdssprob, p = pcopy)
+        nerdsol = solve(newprob, Rosenbrock23(), save_idxs = 1, saveat = 0.01)
+        fit, period, amplitude = CostFunction(nerdsol)
+        
+        if fit == 0.0
+            freqs[i], amps[i] = 0.0, 0.0
+        else 
+            freqs[i], amps[i] = 1/period, amplitude
+        end
+    end
+    return collect(DFs), freqs, amps
 end
 
 
+testnerdssprob = deepcopy(nerdssprob)
+testnerdssprob.p[12] = 1.
+testnerdssprob.u0[1] = 1.0
+DFs, freqs, amps = getFreqvsDF_threaded(testnerdssprob)
 
-function rate_constant_conversion(rate_constant_nm3_per_us::Float64, volume_um3::Float64)
-    # Convert volume from um^3 to L
-    volume_L = volume_um3 * 1e-15
-    # Convert from nm^3/us to L/s
-    rate_constant_L_per_s = rate_constant_nm3_per_us * (1e24) / (1e-6)
-    # Convert from L/s to uM^-1 s^-1 using Avogadro's number and the volume
-    rate_constant_uM_per_s = rate_constant_L_per_s / 6.022e23 / 1e6 / volume_L
-    return rate_constant_uM_per_s
+function plot_freqamp(varrange, freqs, amps, varlabel= "DF (nm)")
+    freqplot = plot(varrange,freqs, xscale=:log10, legend=false, xlabel=varlabel, ylabel="Frequency (Hz)")
+    ampplot = plot(varrange,amps, xscale=:log10, legend=false, xlabel=varlabel, ylabel="Amplitude (uM)")
+    freqvsamp_plot = scatter(freqs, amps, legend=false, xlabel="Frequency", ylabel="Amplitude (uM)", ma=0.4)
+    display(plot(freqplot, ampplot, freqvsamp_plot, plot_title = "$(ogprob.u0[1:4])", layout=(3,1), size=(1000,1000)))
 end
 
-rate_constant_conversion(15.453534109021441e-05, 0.5)
+plot_freqamp(DFs, freqs, amps)
+# freqplot = plot(DFs,freqs, xscale=:log10, legend=false, xlabel="DF (nm)", ylabel="Frequency (Hz)")
+# ampplot = plot(DFs,amps, xscale=:log10, legend=false, xlabel="DF (nm)", ylabel="Amplitude (uM)")
+# freqvsamp_plot = scatter(freqs, amps, legend=false, xlabel="Frequency", ylabel="Amplitude (uM)", ma=0.4)
+# plot(freqplot, ampplot, freqvsamp_plot, plot_title = "$(testnerdssprob.u0[1:4])", layout=(3,1), size=(1000,1000))
+
+
+function getFreqvsConc(nerdssprob, concid1, concid2, concs=0.01:0.01:100.0)
+    prob = deepcopy(nerdssprob)
+    freqs = Vector{Float64}(undef,length(collect(concs)))
+    amps = Vector{Float64}(undef,length(collect(concs)))
+    for (i,conc) in enumerate(concs)
+        prob.u0[concid1] = conc
+        prob.u0[concid2] = conc
+        nerdsol = solve(prob, save_idxs = 1, saveat = 0.01)
+        fit, period, amplitude = CostFunction(nerdsol)
+        
+        if fit == 0.0
+            freqs[i], amps[i] = 0.0, 0.0
+        else 
+            freqs[i], amps[i] = 60/period, amplitude
+        end
+    end
+    return collect(concs), freqs, amps
+end
+
+concs, freqs, amps = getFreqvsConc(testnerdssprob, 2,3)
+plot_freqamp(concs, freqs, amps, "PIP + AP2 (uM)")
+
+testnerdssprob.p[13] = 1000000
+testnerdsol = solve(testnerdssprob, save_idxs = 1)
+getPerAmp(testnerdsol)
+CostFunction(testnerdsol)
+plot(testnerdsol)
+
+
+
+
+
+ogpsym = [:ka1 => 0.009433439939827041, :kb1 => 2.3550169939427845, :kcat1 => 832.7213093872278, 
+        :ka2 => 12.993995997539924, :kb2 => 6.150972501791291,
+        :ka3 => 1.3481451097940793, :kb3 => 0.006201726090609513, 
+        :ka4 => 0.006277294665474662, :kb4 => 0.9250191811994848, 
+        :ka7 => 57.36471615394549, :kb7 => 0.04411989797898752, :kcat7 => 42.288085868394326, 
+        :DF => 2631.050539219606] 
+        
+fastp = [0.13631431320575543, 258.39273861543745, 922.897626015191, 
+        1.7222377782178393, 49.044199345966945, 
+        3.88687247761903, 0.5093036370614539, 
+        0.6602571034403275, 1.3961637922644583, 
+        10.627756359174143, 0.01, 33.6202081364354, 
+        49096.34604920212]
+                                                        
+ogusym = [:L => 1.5, :K => 0.1, :P => 0.29, :A => 1.0, :Lp => 0.0, 
+        :LpA => 0.0, :LK => 0.0, :LpP => 0.0, :LpAK => 0.0, :LpAP => 0.0, :LpAKL => 0.0, :LpAPLp => 0.0, :AK => 0.0, :AP => 0.0, :AKL => 0.0, :APLp => 0.0]
+ogu = [x[2] for x in ogusym]
+
+ogprob = make_ODEProb(fastp, ogu; tspan = (0., 100.))
+ogsol = solve(ogprob, Rosenbrock23(), save_idxs = 1)
+getPerAmp(ogsol)
+plot(ogsol)
+
+
+
+
+
 
 #! run GA to get low copy number oscillations for NERDSS
-ic_constraints = define_initialcondition_constraints(;lipidrange = (100., 500.), kinaserange = (10., 200.), phosphataserange = (10., 200.), ap2range = (10., 500.))
+ic_constraints = define_initialcondition_constraints(lipidrange = (0.1,1.0), kinaserange = (0.01,1.0), phosphataserange = (0.01,1.0), ap2range = (0.01,1.0))
+param_constraints = define_parameter_constraints(karange = (1e-3, 1e2), kbrange = (1e-3, 1e3), kcatrange = (1e-2, 1e3), dfrange = (1e3, 1e6))
 
-nerdss_gaproblem = GAProblem(ic_constraints,nerdssprob)
+ic_gaproblem = GAProblem(ic_constraints,ogprob)
+param_gaproblem = GAProblem(param_constraints,ogprob)
 
-nerdss_record = run_GA(nerdss_gaproblem)
+nerdss_record = run_GA(ic_gaproblem)
+
+
+extract_solution(row) = extract_solution(row, nerdss_record, ogprob; vars = [1], tspan = (0., 50.))
+plotsol(row) = plotsol(row, nerdss_record, ogprob; vars = [1])
+
+plotsol(6)
+
+
+
+
+function calc_concentration_ratio(dfrow::DataFrameRow)
+    ratio = dfrow.ind[1]/sum(dfrow.ind[2:end-1])
+    return round(ratio,digits = 3)
+end
+
+function calc_concentration_ratio(u0::Vector{Float64})
+    ratio = u0[1]/sum(u0[2:end-1])
+    return round(ratio,digits = 3)
+end
+
+minper = argmin(nerdss_record.per)
+minpersol = extract_solution(minper)
+minrow = nerdss_record[minper,:]
+getPerAmp(minpersol)
+CostFunction(minpersol)
+minper_concentration = calc_concentration_ratio(minrow)
+minplot = plot(minpersol, title="Lipid:Enzyme ratio = $minper_concentration", xlabel = "Time (s)", ylabel = "Concentration (μM)", 
+            label = "Frequency = $(round(1/(nerdss_record[minper,:].per),digits = 3)) Hz",legend_font_pointsize=14)
+
+
+maxper = argmax(nerdss_record.per)
+maxpersol = extract_solution(maxper)
+maxrow = nerdss_record[maxper,:]
+getPerAmp(maxpersol)
+CostFunction(maxpersol)
+maxper_concentration = calc_concentration_ratio(maxrow)
+maxplot = plot(maxpersol, title="Lipid:Enzyme ratio = $maxper_concentration",xlabel = "Time (s)", ylabel = "Concentration (μM)",
+                color = :blue, label = "Frequency = $(round(1/(nerdss_record[maxper,:].per),digits = 3)) Hz",legend_font_pointsize=14)
+
+
+minmaxplot = plot(minplot,maxplot, layout = (2,1),bottom_margin = 12px, left_margin = 16px, top_margin = 8px, right_margin = 8px, size = (1000,800),titlefontsize=18, guidefont = 14)
+savefig(minmaxplot, "OscillatorPaper/FigureGenerationScripts/ProgressReportFigures/frequency_compare_plot2.png")
+
+# filter!(x -> x.per < -0.2, nerdss_record)
+
+
+# newpsym = [:ka1 => convert_to_macrorate(0.027267670545107203)#5.453534109021441e-05 #ka1, 1
+#     :kb1 => 0.0643048008980449 #kb1, 2
+#     :kcat1 => 286.6382253193995 #kcat1, 3
+#     :ka2 => convert_to_macrorate(1.6605390671738467) #0.0033210781343476934 #ka2, 4
+#     :kb2 => 0.39569337786534897 #kb2, 5
+#     :ka3 => convert_to_macrorate(0.04115484270564225) #ka3, 6
+#     :kb3 => 0.5393197910059361 #kb3, 7
+#     :ka4 => convert_to_macrorate(0.05448532670350475) #ka4, 8
+#     :kb4 => 0.2897657637531564 #kb4, 9
+#     :ka7 => convert_to_macrorate(0.19013582091039463) #ka7, 10
+#     :kb7 => 0.0028126177315505618 #kb7, 11
+#     :kcat7 => 1.2733781341040291 #kcat7, 12
+#     :DF => 5500.] #DF, 13
+# newp = [x[2] for x in newpsym]
+
+
+# newu0 = [4.9855314928351095, 0.08222720105366571, 0.669026631395984, 3.621476302891451]
+newu0 = [0.9889272736535439, 0.754312949899292, 0.21447637196917263, 0.13832807513023648]
+newp = [0.13631431320575543, 258.39273861543745, 922.897626015191, 
+            1.7222377782178393, 49.044199345966945, 
+                3.88687247761903, 0.5093036370614539, 
+                0.6602571034403275, 1.3961637922644583, 
+                10.627756359174143, 0.01, 33.6202081364354, 
+                49096.34604920212]
+newprob = remake(ogprob; u0 = [newu0; zeros(12)], p = newp, tspan = (0.,100.0))
+newsol = solve(newprob, Rosenbrock23(), save_idxs = 1)
+period, amplitude = getPerAmp(newsol)
+
+plot(newsol, title = "$(calc_concentration_ratio(newu0))", label = "Period = $(round(period,digits = 3)) s\nAmplitude = $(round(amplitude,digits = 3)) uM")
+
+let
+    DFs, freqs, amps = getFreqvsDF_threaded(newprob)
+    plot_freqamp(DFs, freqs, amps)
+end
+
+
+
+
+
+function getFreqvsGrid(nerdssprob, concid1, concid2, concrange1=range(0.1, stop=100.0, length=1000), concrange2=range(10000, stop=100000.0, length=1000))
+    prob = deepcopy(nerdssprob)
+    freqs = Matrix{Float64}(undef,length(collect(concrange1)), length(collect(concrange2)))
+    amps = Matrix{Float64}(undef,length(collect(concrange1)), length(collect(concrange2)))
+    gridprogress = Progress(length(collect(concrange1))*length(collect(concrange2)), dt = 0.1, desc="Evaluating frequency grid... ", color=:red)
+    Threads.@threads for i in eachindex(concrange1)
+        for j in eachindex(concrange2)
+            newu0 = copy(prob.u0)
+            newu0[concid1] = concrange1[i]
+            newp = copy(prob.p)
+            newp[concid2] = concrange2[j]
+            newprob = remake(prob; u0 = newu0, p = newp, tspan = (0.,1000.0))
+            nerdsol = solve(prob, save_idxs = 1, saveat = 0.1)
+            fit, period, amplitude = CostFunction(nerdsol)
+            
+            if fit == 0.0
+                freqs[j,i], amps[j,i] = 0.0, 0.0
+                next!(gridprogress)
+            else 
+                freqs[j,i], amps[j,i] = 60/period, amplitude
+                next!(gridprogress)
+            end
+        end
+    end
+    return collect(concrange1), collect(concrange2), freqs, amps
+end
+
+
+DFs, concs, freqs, amps = getFreqvsGrid(ogprob, 4, 13, range(0.1, stop=100.0, length=1000), range(10000, stop=100000.0, length=1000))
+
+surface(DFs, concs, freqs, xlabel = "DF", ylabel = "AP2", zlabel = "Frequency (1/min)", title = "Frequency vs DF and AP2", color = :plasma)
+
+
+
+
+
+
 
 randprob = remake(nerdssprob; u0 = vcat(nerdss_record[end,:].ind,zeros(12)), tspan = (0.,1000.0))
 randsol = solve(randprob, Rosenbrock23(), saveat = 0.1)
