774f71174caf25165008e3d15776651e8a37d45c
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-06-13 19:27:35 -0400

    default values for fullrn

--- UTILITIES/ReactionNetwork.jl
+++ UTILITIES/ReactionNetwork.jl
@@ -2,7 +2,12 @@
 # const VAR_NAMES = ["L", "K", "P", "A", "Lp", "LpA", "LK", "LpP", "LpAK", "LpAP", "LpAKL", "LpAPLp", "AK", "AP", "AKL", "APLp"]
 
 """Full oscillator model"""
-function make_fullrn()
+function make_fullrn(defparams = [:ka1 => 0.009433439939827041, :kb1 => 2.3550169939427845, :kcat1 => 832.7213093872278, :ka2 => 12.993995997539924, :kb2 => 6.150972501791291,
+                                    :ka3 => 1.3481451097940793, :kb3 => 0.006201726090609513, :ka4 => 0.006277294665474662, :kb4 => 0.9250191811994848, :ka7 => 57.36471615394549, 
+                                        :kb7 => 0.04411989797898752, :kcat7 => 42.288085868394326, :DF => 3631.050539219606],
+                    defvars = [:L => 3.0, :K => 0.5, :P => 0.3, :A => 2.0, :Lp => 0.0, :LpA => 0.0, :LK => 0.0, 
+                        :LpP => 0.0, :LpAK => 0.0, :LpAP => 0.0, :LpAKL => 0.0, :LpAPLp => 0.0, :AK => 0.0, :AP => 0.0, :AKL => 0.0, :APLp => 0.0])
+
     fullrn = @reaction_network fullrn begin
         @parameters ka1 kb1 kcat1 ka2 kb2 ka3 kb3 ka4 kb4 ka7 kb7 kcat7 DF
         @species L(t) K(t) P(t) A(t) Lp(t) LpA(t) LK(t) LpP(t) LpAK(t) LpAP(t) LpAKL(t) LpAPLp(t) AK(t) AP(t) AKL(t) APLp(t)
@@ -36,13 +41,7 @@ function make_fullrn()
         (ka7,kb7), AP + Lp <--> APLp #binding of phosphatase to lipid
         kcat7, APLp --> L + AP #dephosphorylation of lipid
     end  
-    setdefaults!(fullrn, [:ka1 => 0.009433439939827041, :kb1 => 2.3550169939427845, :kcat1 => 832.7213093872278, :ka2 => 12.993995997539924, :kb2 => 6.150972501791291,
-    :ka3 => 1.3481451097940793, :kb3 => 0.006201726090609513, :ka4 => 0.006277294665474662, :kb4 => 0.9250191811994848, :ka7 => 57.36471615394549, 
-    :kb7 => 0.04411989797898752, :kcat7 => 42.288085868394326, :DF => 3631.050539219606,
-    
-    #*initial conditions 
-    :L => 3.0, :K => 0.5, :P => 0.3, :A => 2.0, :Lp => 0.0, :LpA => 0.0, :LK => 0.0, 
-    :LpP => 0.0, :LpAK => 0.0, :LpAP => 0.0, :LpAKL => 0.0, :LpAPLp => 0.0, :AK => 0.0, :AP => 0.0, :AKL => 0.0, :APLp => 0.0])
+    setdefaults!(fullrn, [defparams; defvars])
     return fullrn
 end
 
@@ -69,7 +68,7 @@ end
 
 
 
-#* Function to print out differential equations of a reaction network
+"""Function to print out differential equations of a reaction network in Python format"""
 function format_equations(reactionnetwork::ReactionSystem)
     osys = convert(ODESystem, reactionnetwork)
     for ode in osys.eqs
@@ -83,8 +82,10 @@ function format_equations(reactionnetwork::ReactionSystem)
 end
 
 
-# fullrn = make_fullrn()
-
-# format_equations(fullrn)
-
+"""Automatically render the ODEs of a reaction network in LaTeX"""
+function getlatex_equations(rn)
+    rxnEqs = convert(ODESystem, rn)
+    txt = latexify(rxnEqs)
+    render(txt)
+end
 
