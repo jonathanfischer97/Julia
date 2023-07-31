5b418dcbe10b4605f189f9504c19229ba06dee1c
jonathanfischer97 <jfisch27@jhu.edu>
2023-07-31 15:38:02 -0400

    i

---
+++ OscillatorPaper/FigureGenerationScripts/InitialConditionCSVmaker.jl
@@ -0,0 +1,66 @@
+begin 
+    using Plots; #theme(:juno)
+    using Catalyst
+    using DifferentialEquations
+    using Statistics
+    using Peaks
+    using Evolutionary, FFTW
+    using Random
+    using Distributions
+    using DataFrames
+    using CSV
+    # using Unitful
+    # using Unitful: µM, M, nm, µm, s, μs, Na, L, 𝐍
+    using StaticArrays
+    using BenchmarkTools, Profile, ProgressMeter
+    # using Cthulhu
+    # using JET
+    # using MultivariateStats, UMAP, TSne, StatsPlots
+    # using GlobalSensitivity, QuasiMonteCarlo
+    using LinearAlgebra
+    # using BifurcationKit, Setfield, ForwardDiff, Parameters; const BK = BifurcationKit
+    using ColorSchemes, Plots.PlotMeasures
+    # using OrderedCollections
+    # using Combinatorics
+    # using LazySets, Polyhedra
+    default(lw = 2, size = (1000, 600), dpi = 200, bottom_margin = 12px, left_margin = 16px, top_margin = 10px, right_margin = 8px)
+    # plotlyjs()
+    # import CairoMakie as cm 
+    # gr()
+    # push!(LOAD_PATH, "../../UTILITIES")
+
+    include("../../UTILITIES/EvolutionaryOverloads.jl")
+
+    # import the Catalyst model "fullrn"
+    include("../../UTILITIES/ReactionNetwork.jl")
+
+    # import the cost function and other evaluation functions
+    include("../../UTILITIES/EvaluationFunctions.jl")
+    # using .EvaluationFunctions
+
+    # import the genetic algorithm and associated functions
+    include("../../UTILITIES/GA_functions.jl")
+
+    include("../../UTILITIES/ODEProbMaker.jl")
+
+    include("../../UTILITIES/UnitTools.jl")
+
+
+    const SHOW_PROGRESS_BARS = parse(Bool, get(ENV, "PROGRESS_BARS", "true"))
+end
+
+
+fullrn = make_fullrn()
+ogprob = ODEProblem(fullrn, [], (0.,1000.0), [])
+
+ogsol = solve(ogprob, Rosenbrock23(), save_idxs = 1)
+plot(ogsol)
+
+
+#* Optimization of initial conditions to produce data for CSV
+ic_constraints = define_initialcondition_constraints(lipidrange = (0.1,10.), kinaserange = (0.01,10.), phosphataserange = (0.01,10.), ap2range = (0.01,10.))
+
+ic_gaproblem = GAProblem(ic_constraints,ogprob)
+
+ic_record = run_GA(ic_gaproblem)
+CSV.write("OscillatorPaper/FigureGenerationScripts/initialconditions.csv", ic_record)
