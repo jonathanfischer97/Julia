743c387118a2a0c720e534427dec3495659f8f0c
jonathanfischer97 <jfisch27@jhu.edu>
2023-08-03 12:09:25 -0400

    works with 100s

--- OscillatorPaper/FigureGenerationScripts/3FixedParamCSVMaker.jl
+++ OscillatorPaper/FigureGenerationScripts/3FixedParamCSVMaker.jl
@@ -51,7 +51,7 @@ end
 
 
 fullrn = make_fullrn()
-ogprob = ODEProblem(fullrn, [], (0.,1000.0), [])
+ogprob = ODEProblem(fullrn, [], (0.,100.0), [])
 # @benchmark solve($ogprob, saveat = 0.1, save_idxs = 1)
 
 # ogsol = solve(ogprob, Rosenbrock23(), save_idxs = 1)
@@ -59,7 +59,7 @@ ogprob = ODEProblem(fullrn, [], (0.,1000.0), [])
 
 
 #* Optimization of parameters to produce data for CSV
-param_constraints = define_parameter_constraints(karange = (1e-3, 1e2), kbrange = (1e-3, 1e3), kcatrange = (1e-2, 1e3), dfrange = (1e3, 2e5))
+param_constraints = define_parameter_constraints(karange = (1e-3, 1e2), kbrange = (1e-3, 1e3), kcatrange = (1e-2, 1e3), dfrange = (1e3, 2e4))
 
 
 # param_gaproblem = GAProblem(param_constraints,ogprob)
@@ -198,6 +198,7 @@ function fixed_triplet_csv_maker(param1::String, param2::String, param3::String,
         for val2 in fixed_values2
             for val3 in fixed_values3
                 fixed_values = [val1, val2, val3]
+                @info fixed_values
                 make_fitness_function_closure(evalfunc,prob) = make_fitness_function_with_fixed_inputs(evalfunc, prob, fixed_values, fixedtrip_idxs)
                 oscillatory_points_df = run_GA(fixed_ga_problem, make_fitness_function_closure; population_size = 1000, iterations = 5) 
 
@@ -211,6 +212,8 @@ function fixed_triplet_csv_maker(param1::String, param2::String, param3::String,
     return results_df
 end
 
-results_df = fixed_triplet_csv_maker("ka1", "ka2", "ka3", param_constraints, ogprob)
+results_df = fixed_triplet_csv_maker("kcat1", "kcat7", "DF", param_constraints, ogprob)
+results_df = DataFrame(kcat1 = results_df.fixed_value1, kcat7 = results_df.fixed_value2, DF = results_df.fixed_value3, average_period = results_df.average_period, average_amplitude = results_df.average_amplitude)
+CSV.write("OscillatorPaper/FigureGenerationScripts/fixed_triplet_results.csv", results_df)
 
 @code_warntype fixed_triplet_csv_maker("ka1", "ka2", "ka3", param_constraints, ogprob)
