d3c158f4f3c967306c4f7807d251c88db03ffcb3
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-08-28 20:51:36 -0400

    no more nominal vals. Log sampling for all

--- UTILITIES/GA_functions.jl
+++ UTILITIES/GA_functions.jl
@@ -16,10 +16,9 @@ Struct for defining parameter ranges. Each instance contains a name, and a range
 """
 struct ConstraintRange
     name::String
-    symbol::Symbol
     min::Float64
     max::Float64
-    nominal::Float64
+    # nominal::Union{Nothing, Float64}
 end
 
 """
@@ -47,10 +46,6 @@ Struct encapsulating all constraints. Each field represents a different paramete
 """
 mutable struct AllConstraints <: ConstraintType
     ranges::Vector{ConstraintRange}
-
-    # function AllConstraints(paramconstraints, icconstraints) 
-    #     new(paramconstraints.ranges, icconstraints.ranges)
-    # end
 end
 #> END 
 
@@ -72,39 +67,34 @@ constraints = define_parameter_constraints(
 )
 ```
 """
-function define_parameter_constraints(; karange = (1e-3, 1e1), kbrange = (1e-3, 1e3), kcatrange = (1e-3, 1e3), dfrange = (1e3, 1e5),
-    nominalvals = (;ka1 = 0.009433439939827041, kb1 = 2.3550169939427845, kcat1 = 832.7213093872278, ka2 = 12.993995997539924, kb2 = 6.150972501791291,
-            ka3 = 1.3481451097940793, kb3 = 0.006201726090609513, ka4 = 0.006277294665474662, kb4 = 0.9250191811994848, ka7 = 57.36471615394549, 
-            kb7 = 0.04411989797898752, kcat7 = 42.288085868394326, DF = 3631.050539219606)
-            )
+function define_parameter_constraints(; karange = (1e-3, 1e1), kbrange = (1e-3, 1e3), kcatrange = (1e-3, 1e3), dfrange = (1e3, 1e5))#, nominalvals = repeat([Nothing],13))
     #* Define parameter constraint ranges
     ka_min, ka_max = karange  # uM^-1s^-1, log scale
     kb_min, kb_max = kbrange  # s^-1, log scale
     kcat_min, kcat_max = kcatrange # s^-1, log scale
     df_min, df_max = dfrange # for DF, log scale
 
-
     return ParameterConstraints(
         [
-        ConstraintRange("ka1", :ka1, ka_min, ka_max, nominalvals[1]),
-        ConstraintRange("kb1", :kb1, kb_min, kb_max, nominalvals[2]),
-        ConstraintRange("kcat1", :kcat1, kcat_min, kcat_max, nominalvals[3]),
-        ConstraintRange("ka2", :ka2, ka_min, ka_max, nominalvals[4]),
-        ConstraintRange("kb2", :kb2, kb_min, kb_max, nominalvals[5]),
-        ConstraintRange("ka3", :ka3, ka_min, ka_max, nominalvals[6]),
-        ConstraintRange("kb3", :kb3, kb_min, kb_max, nominalvals[7]),
-        ConstraintRange("ka4", :ka4, ka_min, ka_max, nominalvals[8]),
-        ConstraintRange("kb4", :kb4, kb_min, kb_max, nominalvals[9]),
-        ConstraintRange("ka7", :ka7, ka_min, ka_max, nominalvals[10]),
-        ConstraintRange("kb7", :kb7, kb_min, kb_max, nominalvals[11]),
-        ConstraintRange("kcat7", :kcat7, kcat_min, kcat_max, nominalvals[12]),
-        ConstraintRange("DF", :DF, df_min, df_max, nominalvals[13])
+        ConstraintRange("ka1", ka_min, ka_max),
+        ConstraintRange("kb1", kb_min, kb_max),
+        ConstraintRange("kcat1", kcat_min, kcat_max),
+        ConstraintRange("ka2", ka_min, ka_max),
+        ConstraintRange("kb2", kb_min, kb_max),
+        ConstraintRange("ka3", ka_min, ka_max),
+        ConstraintRange("kb3", kb_min, kb_max),
+        ConstraintRange("ka4", ka_min, ka_max),
+        ConstraintRange("kb4", kb_min, kb_max),
+        ConstraintRange("ka7", ka_min, ka_max),
+        ConstraintRange("kb7", kb_min, kb_max),
+        ConstraintRange("kcat7", kcat_min, kcat_max),
+        ConstraintRange("DF", df_min, df_max)
         ]
     )
 end
 
-define_parameter_constraints(prob::ODEProblem; karange = (1e-3, 1e1), kbrange = (1e-3, 1e3), kcatrange = (1e-3, 1e3), dfrange = (1e3, 1e5)) = 
-                        define_parameter_constraints(; karange=karange, kbrange=kbrange, kcatrange=kcatrange, dfrange=dfrange, nominalvals = prob.p)
+# define_parameter_constraints(prob::ODEProblem; karange = (1e-3, 1e1), kbrange = (1e-3, 1e3), kcatrange = (1e-3, 1e3), dfrange = (1e3, 1e5)) = 
+#                         define_parameter_constraints(; karange=karange, kbrange=kbrange, kcatrange=kcatrange, dfrange=dfrange, nominalvals = prob.p)
 
 
 """
@@ -122,9 +112,7 @@ constraints = define_initialcondition_constraints(
 )
 ```
 """
-function define_initialcondition_constraints(;Lrange = (0.1, 10.0), Krange = (0.1, 5.0), Prange = (0.1, 5.0), Arange = (0.1, 10.0),
-                                                nominalvals = (;L = 3.0, K = 0.5, P = 0.3, A = 2.0)
-                                            )
+function define_initialcondition_constraints(;Lrange = (0.1, 10.0), Krange = (0.1, 5.0), Prange = (0.1, 5.0), Arange = (0.1, 10.0))#, nominalvals = repeat([Nothing],4))
     # Define parameter constraint ranges
     lipid_min, lipid_max = Lrange  # uM
     kinase_min, kinase_max = Krange  # uM
@@ -133,16 +121,17 @@ function define_initialcondition_constraints(;Lrange = (0.1, 10.0), Krange = (0.
 
     return InitialConditionConstraints(
         [
-        ConstraintRange("PIP+PIP2", :L, lipid_min, lipid_max, nominalvals[1]),
-        ConstraintRange("Kinase", :K, kinase_min, kinase_max, nominalvals[2]),
-        ConstraintRange("Phosphatase", :P, phosphatase_min, phosphatase_max, nominalvals[3]),
-        ConstraintRange("AP2", :A, ap2_min, ap2_max, nominalvals[4])
+        ConstraintRange("L", lipid_min, lipid_max),
+        ConstraintRange("K", kinase_min, kinase_max),
+        ConstraintRange("P", phosphatase_min, phosphatase_max),
+        ConstraintRange("A", ap2_min, ap2_max)
         ]
     )
 end
 
-define_initialcondition_constraints(prob::ODEProblem; Lrange = (0.1, 10.0), Krange = (0.1, 5.0), Prange = (0.1, 5.0), Arange = (0.1, 10.0)) = 
-                                    define_initialcondition_constraints(;Lrange=Lrange, Krange=Krange, Prange= Prange, Arange=Arange, nominalvals = prob.u0[1:4])
+# define_initialcondition_constraints(prob::ODEProblem; Lrange = (0.1, 10.0), Krange = (0.1, 5.0), Prange = (0.1, 5.0), Arange = (0.1, 10.0)) = 
+#                                     define_initialcondition_constraints(;Lrange=Lrange, Krange=Krange, Prange= Prange, Arange=Arange, nominalvals = prob.u0[1:4])
+
 
 
 function AllConstraints(paramconstraints::ParameterConstraints, icconstraints::InitialConditionConstraints) 
@@ -207,7 +196,7 @@ constraints = define_parameter_constraints(karange = (-3.0, 1.0), kbrange = (-3.
 population = generate_population(constraints, 100)
 ```
 """
-function generate_population(constraint::ParameterConstraints, n::Int)
+function generate_population(constraint::ConstraintType, n::Int)
     population = [exp10.(rand(Uniform(log10(conrange.min), log10(conrange.max)), n)) for conrange in constraint.ranges]
     population = transpose(hcat(population...))
     return [population[:, i] for i in 1:n]
@@ -224,22 +213,22 @@ constraints = define_initialcondition_constraints(lipidrange = (0.1, 10.0), kina
 population = generate_population(constraints, 100)
 ```
 """
-function generate_population(constraint::InitialConditionConstraints, n::Int)
-    population = [rand(Uniform(conrange.min, conrange.max), n) for conrange in constraint.ranges]
-    population = transpose(hcat(population...))
-    return [population[:, i] for i in 1:n]
-end
+# function generate_population(constraint::InitialConditionConstraints, n::Int)
+#     population = [rand(Uniform(conrange.min, conrange.max), n) for conrange in constraint.ranges]
+#     population = transpose(hcat(population...))
+#     return [population[:, i] for i in 1:n]
+# end
 
-function generate_population(constraint::AllConstraints, n::Int)
-    #* find index where initial conditions start, will be named either L, K, P, or A
-    icstartidx = findfirst(x -> x.symbol ∈ [:L, :K, :P, :A], constraint.ranges)
-    parampopulation = [exp10.(rand(Uniform(log10(conrange.min), log10(conrange.max)), n)) for conrange in constraint.ranges[1:icstartidx-1]]
-    icpopulation = [rand(Uniform(conrange.min, conrange.max), n) for conrange in constraint.ranges[icstartidx:end]]
-    parampopulation = transpose(hcat(parampopulation...))
-    icpopulation = transpose(hcat(icpopulation...))
-    return [[parampopulation[:,i]; icpopulation[:,i]] for i in 1:n]
-    # return [population[:, i] for i in 1:n]
-end
+# function generate_population(constraint::AllConstraints, n::Int)
+#     #* find index where initial conditions start, will be named either L, K, P, or A
+#     icstartidx = findfirst(x -> x.name ∈ ["L", "K", "P", "A"], constraint.ranges)
+#     parampopulation = [exp10.(rand(Uniform(log10(conrange.min), log10(conrange.max)), n)) for conrange in constraint.ranges[1:icstartidx-1]]
+#     icpopulation = [rand(Uniform(conrange.min, conrange.max), n) for conrange in constraint.ranges[icstartidx:end]]
+#     parampopulation = transpose(hcat(parampopulation...))
+#     icpopulation = transpose(hcat(icpopulation...))
+#     return [[parampopulation[:,i]; icpopulation[:,i]] for i in 1:n]
+#     # return [population[:, i] for i in 1:n]
+# end
 
 """For calculating volume when optimizing for NERDSS solutions"""
 function generate_population(constraint::Vector{ConstraintRange}, n::Int)
@@ -252,23 +241,23 @@ end
 
 #< DEFAULT FITNESS FUNCTION FACTORY
 """Returns the `fitness function(input)` for the cost function, referencing the ODE problem with closure"""
-function make_fitness_function(evalfunc::Function, prob::ODEProblem; fitidx::Int = 4)
+function make_fitness_function(evalfunc::Function, prob::ODEProblem)
     function fitness_function(input::Vector{Float64})
         #? Returns a cost function method that takes in just a vector of parameters/ICs and references the ODE problem 
-        return evalfunc(input, prob; idx = fitidx)
+        return evalfunc(input, prob)
     end
     return fitness_function
 end
 
-function make_fitness_function_with_let(evalfunc::Function, prob::ODEProblem; fitidx::Int = 4)
-    let evalfunc = evalfunc, prob = prob, fitidx = fitidx
+"""Returns the `fitness function(input)` for the cost function, referencing the ODE problem with closure, captured with let keyword"""
+function make_fitness_function_with_let(evalfunc::Function, prob::ODEProblem)
+    let evalfunc = evalfunc, prob = prob
         function fitness_function(input::Vector{Float64})
-            return evalfunc(input, prob; idx = fitidx)
+            return evalfunc(input, prob)
         end
         return fitness_function
     end
 end
-
 #> END
 
 
@@ -291,7 +280,7 @@ end
 Runs the genetic algorithm, returning the `result`, and the `record` named tuple
 """
 function run_GA(ga_problem::GAProblem, fitnessfunction_factory::Function=make_fitness_function; 
-                                            threshold=10000, population_size = 5000, abstol=1e-4, reltol=1e-2, successive_f_tol = 2, iterations=5, parallelization = :thread, fitidx = 4, show_trace=true)
+                                            threshold=10000, population_size = 5000, abstol=1e-4, reltol=1e-2, successive_f_tol = 2, iterations=5, parallelization = :thread, show_trace=true)
     blas_threads = BLAS.get_num_threads()
     BLAS.set_num_threads(1)
 
@@ -314,12 +303,12 @@ function run_GA(ga_problem::GAProblem, fitnessfunction_factory::Function=make_fi
     mutation_range = fill(mutation_scalar, length(ga_problem.constraints.ranges))
 
     #* Define the GA method.
-    mthd = GA(populationSize = population_size, selection = tournament(Int(population_size/10)),
+    mthd = GA(populationSize = population_size, selection = tournament(Int(population_size/10), select=argmax),
     crossover = TPX, crossoverRate = 1.0, # Two-point crossover event
     mutation  = BGA(mutation_range, 2), mutationRate = 1.0)
 
     #* Make fitness function. Makes closure of evaluation function and ODE problem
-    fitness_function = fitnessfunction_factory(ga_problem.eval_function, ga_problem.ode_problem; fitidx = fitidx)
+    fitness_function = fitnessfunction_factory(ga_problem.eval_function, ga_problem.ode_problem)
 
     #* Run the optimization.
     result = Evolutionary.optimize(fitness_function, [0.0,0.0,0.0], boxconstraints, mthd, pop, opts)
@@ -392,11 +381,6 @@ end
 """Defines logspace function for sampling parameters"""
 logrange(start, stop, length::Int) = exp10.(collect(range(start=log10(start), stop=log10(stop), length=length)))
 
-"""Extract solution of a row from the dataframe"""
-function extract_solution(row, df::DataFrame, prob::ODEProblem; vars::Vector{Int} = collect(1:length(prob.u0)), tspan = (0.0, 100.0))
-    reprob = length(df.ind[row]) > 4 ? remake(prob, p = df.ind[row], tspan = tspan) : remake(prob, u0 = [df.ind[row]; zeros(length(prob.u0) - length(df.ind[row]))], tspan = tspan)
-    solve(reprob, Rosenbrock23(), save_idxs = vars)
-end
 
 
 """Splits ind column into separate columns for each parameter, adds initial conditions for writing DataFrame to CSV"""
@@ -435,9 +419,6 @@ function split_dataframe!(df, prob)
         for (i,conc) in enumerate(allsymbols)
             df[!, conc] .= [x[i] for x in df.ind]
         end
-        # for (i,param) in enumerate(paramsymbols)
-        #     df[!, param] .= [x[i] for x in df.ind[5:end]]
-        # end
         select!(df, Not(:ind))
     end
 end
