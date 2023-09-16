f5f3cff5c1cdd685ab7feb7ec4886cfd57775cf9
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-09-16 18:54:46 -0400

    oops

--- OscillatorPaper/FigureGenerationScripts/core_num_tester.sh
+++ OscillatorPaper/FigureGenerationScripts/core_num_tester.sh
@@ -3,6 +3,6 @@
 # Loop through values from 1 to 36 with steps of 4
 for i in $(seq 1 4 36); do
   # Submit a new SLURM job with $i cores
-  sbatch --ntasks=$i --export=JULIA_NUM_THREADS=$i run_julia_job.sh
+  sbatch --ntasks=$i --export=JULIA_NUM_THREADS=$i run_core_test.sh
 done
 
