6a18392fddf26c757a0ef108ef9a1b3ebf183a4e
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-09-17 15:50:57 -0400

    q

--- OscillatorPaper/FigureGenerationScripts/core_num_tester.sh
+++ OscillatorPaper/FigureGenerationScripts/core_num_tester.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Loop through values from 1 to 36 with steps of 4
-for i in $(seq 1 4 48); do
-  # Submit a new SLURM job with $i cores
-  sbatch --ntasks=$i --export=JULIA_NUM_THREADS=$i run_core_test.sh
-done
+for i in $(seq 1 4 36); do
+  # Generate a unique filename for each job's output
+  unique_output_file="TimerResults/timeresults_${i}.txt"
 
+  # Submit the job to SLURM with the specified number of threads
+  sbatch --job-name="CoreTest_$i" --ntasks-per-node=$i --wrap="julia --threads=$i CoreNumTester.jl >> $unique_output_file"
+
+  # Add a separator line for readability in the output file
+  echo "------------------" >> $unique_output_file
+done
