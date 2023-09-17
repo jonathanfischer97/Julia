38409db48ead522e09a3abd9d84e0b7c5e6d8d1f
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-09-17 19:25:41 -0400

    w

--- OscillatorPaper/FigureGenerationScripts/core_num_tester.sh
+++ OscillatorPaper/FigureGenerationScripts/core_num_tester.sh
@@ -6,7 +6,7 @@ for i in $(seq 1 4 48); do
   unique_output_file="TimerResults/timeresults_${i}.txt"
 
   # Submit the job to SLURM with the specified number of threads
-  sbatch --output=$unique_output_file --job-name="CoreTest_$i" --ntasks-per-node=$i run_core_test.sh
+  sbatch --output=$unique_output_file --job-name="C_$i" --ntasks-per-node=$i run_core_test.sh
 
   # Add a separator line for readability in the output file
   echo "------------------" >> $unique_output_file
