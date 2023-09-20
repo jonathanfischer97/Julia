a9cb16fa82f412a77f19a22c773890c3d8321207
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2023-09-20 15:25:07 -0400

    cluster

--- OscillatorPaper/FixedConstraintAnalysis/test/TestBench.jl
+++ OscillatorPaper/FixedConstraintAnalysis/test/TestBench.jl
@@ -31,6 +31,7 @@ begin
 
     const SHOW_PROGRESS_BARS = parse(Bool, get(ENV, "PROGRESS_BARS", "true"))
 
+    using FFTW
     FFTW.set_num_threads(18)
 end
 
@@ -39,5 +40,37 @@ end
 #* Clustering test 
 dfarray = read_csvs_in_directory("/home/local/WIN/jfisch27/Desktop/Julia/OscillatorPaper/FigureGenerationScripts/ROCKFISH_DATA/4Fixed/PopSize_10000/4FixedICRawSets/DF=1000.0")
 
+import Clustering: kmeans
+function kmeans(df::DataFrame, k::Int; exclude_cols::Vector{Symbol} = Symbol[])
+    included_cols = setdiff(propertynames(df), exclude_cols)
+    data_matrix = Matrix(df[:, included_cols])
+    return kmeans(data_matrix, k)
+end
 
+function elbow_method(df::DataFrame, max_k::Int; exclude_cols::Vector{Symbol} = Symbol[])
+    included_cols = setdiff(propertynames(df), exclude_cols)
+    data_matrix = Matrix(df[:, included_cols])
+    return elbow_method(data_matrix, max_k)
+end
 
+function elbow_method(X::Matrix{Float64}, max_k::Int)
+    distortions = Vector{Float64}(undef, max_k)
+    for k in 1:max_k
+        result = kmeans(X, k)
+        distortions[k] = totalcost(result)
+    end
+    return distortions
+end
+
+ 
+clusterarray = [kmeans(df, 4, exclude_cols = [:fit, :per, :amp, :DF, :L, :K, :P, :A]) for df in dfarray]
+
+df = dfarray[1]
+cluster = clusterarray[1]
+
+a = assignments(cluster)
+c = counts(cluster)
+m = cluster.centers
+n = nclusters(cluster)
+
+plot(df.ka1, df.kb1, marker_z = a, color=:lightrainbow)
